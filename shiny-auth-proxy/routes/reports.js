106c65c3ebc59a483c793e4611a5b0096d66360b
Kerin Cosford <kerin.cosford@digital.justice.gov.uk>
2017-01-18 16:47:21 +0000

    Sorted out auth proxy submodule weirdness

---
+++ shiny-auth-proxy/routes/reports.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var passport = require('passport');
+var httpProxy = require('http-proxy');
+var ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn()
+var router = express.Router();
+
+var proxy = httpProxy.createProxyServer({
+  target: {
+      host: process.env.SHINY_HOST,
+      port: process.env.SHINY_PORT
+    }
+});
+
+proxy.on('error', function(e) {
+  console.log('Error connecting');
+  console.log(e);
+});
+
+var setIfExists = function(proxyReq, header, value){
+  if(value){
+    proxyReq.setHeader(header, value);
+  }
+}
+
+proxy.on('proxyReq', function(proxyReq, req, res, options) {
+  setIfExists(proxyReq, 'x-auth0-nickname', req.user._json.nickname);
+  setIfExists(proxyReq, 'x-auth0-user_id', req.user._json.user_id);
+  setIfExists(proxyReq, 'x-auth0-email', req.user._json.email);
+  setIfExists(proxyReq, 'x-auth0-name', req.user._json.name);
+  setIfExists(proxyReq, 'x-auth0-picture', req.user._json.picture);
+  setIfExists(proxyReq, 'x-auth0-locale', req.user._json.locale);
+});
+
+/* Proxy all requests */
+router.all(/.*/, ensureLoggedIn, function(req, res, next) {
+  proxy.web(req, res);
+});
+
+module.exports = router;
