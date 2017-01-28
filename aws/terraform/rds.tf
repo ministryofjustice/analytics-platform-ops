c29634e67e4b4ad331f8ff17f86e6793aa95eb54
Kerin Cosford <cosford@gmail.com>
2017-01-28 15:31:35 +0000

    Added in-progress Terraform RDS resource

--- aws/terraform/rds.tf
+++ aws/terraform/rds.tf
@@ -1,25 +1,25 @@
-resource "aws_db_subnet_group" "main" {
-  name = "main"
-  subnet_ids = ["${aws_subnet.rds.*.id}"]
-  tags = {
-    KubernetesCluster = "${var.cluster_name}"
-    Name = "rds.${var.cluster_name}"
-  }
-}
+# resource "aws_db_subnet_group" "main" {
+#   name = "main"
+#   subnet_ids = ["${aws_subnet.storage.*.id}"]
+#   tags = {
+#     KubernetesCluster = "${var.cluster_name}"
+#     Name = "rds.${var.cluster_name}"
+#   }
+# }
 
-resource "aws_db_instance" "main" {
-  identifier           = "main-postgresql"
-  allocated_storage    = 10
-  engine               = "postgresql"
-  engine_version       = "9.6.1"
-  instance_class       = "db.t1.micro"
-  name                 = "main"
-  username             = "${var.main_db_username}"
-  password             = "${var.main_db_password}"
-  db_subnet_group_name = "${aws_db_subnet_group.main.name}"
+# resource "aws_db_instance" "main" {
+#   identifier           = "main-postgresql"
+#   allocated_storage    = 10
+#   engine               = "postgresql"
+#   engine_version       = "9.6.1"
+#   instance_class       = "db.t1.micro"
+#   name                 = "main"
+#   username             = "${var.main_db_username}"
+#   password             = "${var.main_db_password}"
+#   db_subnet_group_name = "${aws_db_subnet_group.main.name}"
 
-  tags = {
-    KubernetesCluster = "${var.cluster_name}"
-    Name = "main.rds.${var.cluster_name}"
-  }
-}
+#   tags = {
+#     KubernetesCluster = "${var.cluster_name}"
+#     Name = "main.rds.${var.cluster_name}"
+#   }
+# }
