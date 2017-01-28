b38541e1277a13a677a37b3c093335b95c02c631
Kerin Cosford <cosford@gmail.com>
2017-01-28 15:30:52 +0000

    Added Terraform resource for storage (EFS, RDS, etc) subnets

--- aws/terraform/network.tf
+++ aws/terraform/network.tf
@@ -1,12 +1,12 @@
-resource "aws_subnet" "rds" {
+resource "aws_subnet" "storage" {
   vpc_id = "${var.vpc_id}"
-  cidr_block = "${element(var.cidr_blocks_rds, count.index)}"
+  cidr_block = "${element(var.cidr_blocks_storage, count.index)}"
   availability_zone = "${element(var.zones, count.index)}"
   count = "${length(var.zones)}"
 
   tags = {
     KubernetesCluster = "${var.cluster_name}"
-    Name = "rds-${element(var.zones, count.index)}.${var.cluster_name}"
+    Name = "storage-${element(var.zones, count.index)}.${var.cluster_name}"
   }
 }
 
