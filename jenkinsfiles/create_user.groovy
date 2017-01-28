b308cad6c7e5f91ae7e54ff4cb56fd8b6e9df357
Kerin Cosford <cosford@gmail.com>
2017-01-28 17:45:17 +0000

    Attempting to simplify user setup

--- jenkinsfiles/create_user.groovy
+++ jenkinsfiles/create_user.groovy
@@ -1,18 +1,19 @@
 node {
-    stage ("Create namespace") {
-sh """
-cat <<EOF | kubectl apply -f -
-apiVersion: v1
-kind: Namespace
-metadata:
-  name: user-${env.USERNAME}
-EOF
-"""
+    stage ("Apply default namespace resources") {
+        sh """
+        for f in k8s-templates/user-resources/default-namespace/*
+        do
+            cat \$f | sed \\
+                -e 's/{{\\.Username}}/${env.USERNAME}/g' \\
+                -e 's/{{\\.EFSHostname}}/${env.EFS_HOSTNAME}/g' | \\
+            kubectl apply -n user-${env.USERNAME} -f -
+        done
+        """
     }
 
-    stage ("Apply kubernetes resources") {
+    stage ("Apply user namespace resources") {
         sh """
-        for f in k8s-templates/user-resources/*
+        for f in k8s-templates/user-resources/user-namespace/*
         do
             cat \$f | sed \\
                 -e 's/{{\\.Username}}/${env.USERNAME}/g' \\
