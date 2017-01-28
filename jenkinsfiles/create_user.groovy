68eab61a27539e5a61a6c339c86157e228cfdbc3
Kerin Cosford <cosford@gmail.com>
2017-01-28 16:51:55 +0000

    Added jenkinsfiles dir

---
+++ jenkinsfiles/create_user.groovy
@@ -0,0 +1,29 @@
+node {
+
+    stage ("Checkout") {
+        git url: 'https://github.com/ministryofjustice/analytics-qnd-ops.git'
+    }
+
+    stage ("Create namespace") {
+sh """
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: Namespace
+metadata:
+  name: user-${env.USERNAME}
+EOF
+"""
+    }
+
+    stage ("Apply kubernetes resources") {
+        sh """
+        for f in k8s-templates/user-resources/*
+        do
+            cat \$f | sed \\
+                -e 's/{{\\.Username}}/${env.USERNAME}/g' \\
+                -e 's/{{\\.EFSHostname}}/${env.EFS_HOSTNAME}/g' | \\
+            kubectl apply -n user-${env.USERNAME} -f -
+        done
+        """
+    }
+}
