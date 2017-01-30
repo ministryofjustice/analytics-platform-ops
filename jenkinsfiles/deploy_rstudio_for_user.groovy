629813a809057975535fe679e510cf5abd8e30c1
Kerin Cosford <kerin.cosford@digital.justice.gov.uk>
2017-01-30 14:28:20 +0000

    Added Jenkins RStudio deploy script

---
+++ jenkinsfiles/deploy_rstudio_for_user.groovy
@@ -0,0 +1,21 @@
+node {
+
+    git "https://github.com/ministryofjustice/analytics-qnd-ops.git"
+
+    stage ("Deploy RStudio") {
+        sh """
+        for f in k8s-templates/r-studio-user/*
+        do
+            USERNAME=${env.USERNAME}
+            CALLBACK_URL=$(echo -n "https://r-studio.${env.USERNAME}.users.analytics.kops.integration.dsd.io/callback"|base64)
+            COOKIE_SECRET=$(echo -n "${env.COOKIE_SECRET}"|base64)
+
+            cat \$f | sed \\
+                -e s/{{\\.Username}}/\${USERNAME}/g \\
+                -e s/{{\\.CallbackURLB64}}/\${CALLBACK_URL}/g \\
+                -e s/{{\\.CookieSecretB64}}/\${COOKIE_SECRET}/g | \\
+            kubectl apply -f -
+        done
+        """
+    }
+}
