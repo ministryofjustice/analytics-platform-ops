8e3c74f4b3abe416f62bc01ef08d53e48f318b5a
Kerin Cosford <kerin.cosford@digital.justice.gov.uk>
2017-01-30 15:07:18 +0000

    Fixed base64 breaking output at col 76

--- jenkinsfiles/deploy_rstudio_for_user.groovy
+++ jenkinsfiles/deploy_rstudio_for_user.groovy
@@ -4,14 +4,13 @@ node {
 
     stage ("Deploy RStudio") {
         sh """
-        USERNAME=${env.USERNAME}
-        CALLBACK_URL=$(echo -n "https://r-studio.${env.USERNAME}.users.analytics.kops.integration.dsd.io/callback"|base64)
-        COOKIE_SECRET=$(echo -n "${env.COOKIE_SECRET}"|base64)
+        CALLBACK_URL=\$(echo -n "https://r-studio.${env.USERNAME}.users.analytics.kops.integration.dsd.io/callback"|base64 -w 0)
+        COOKIE_SECRET=\$(echo -n "${env.COOKIE_SECRET}"|base64 -w 0)
 
         for f in k8s-templates/r-studio-user/*
         do
             cat \$f | sed \\
-                -e s/{{\\.Username}}/\${USERNAME}/g \\
+                -e s/{{\\.Username}}/${env.USERNAME}/g \\
                 -e s/{{\\.CallbackURLB64}}/\${CALLBACK_URL}/g \\
                 -e s/{{\\.CookieSecretB64}}/\${COOKIE_SECRET}/g | \\
             kubectl apply -f -
