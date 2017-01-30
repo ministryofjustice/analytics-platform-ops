51a1ff14944b707502032e7395abe12bd2238611
Kerin Cosford <kerin.cosford@digital.justice.gov.uk>
2017-01-30 13:45:40 +0000

    Trying to fix syntax

--- jenkinsfiles/create_rstudio_config.groovy
+++ jenkinsfiles/create_rstudio_config.groovy
@@ -7,9 +7,9 @@ node {
         for f in k8s-templates/r-studio-config/*
         do
             cat \$f | sed \\
-                -e "s/{{\\.ClientSecretB64}}/\$(echo -n ${env.CLIENT_SECRET}|base64)/g" \\
-                -e "s/{{\\.ClientIDB64}}/\$(echo -n ${env.CLIENT_ID}|base64)/g" \\
-                -e "s/{{\\.Domain}}/\$(echo -n ${env.DOMAIN}|base64)/g" | \\
+                -e s/{{\\.ClientSecretB64}}/"\$(echo -n ${env.CLIENT_SECRET}|base64)"/g \\
+                -e s/{{\\.ClientIDB64}}/"\$(echo -n ${env.CLIENT_ID}|base64)"/g \\
+                -e s/{{\\.Domain}}/"\$(echo -n ${env.DOMAIN}|base64)"/g | \\
             kubectl apply -f -
         done
         """
