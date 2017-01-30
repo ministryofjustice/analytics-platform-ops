4e689b3baf5e79bf9e2d465e4cb71b6344ccb923
Kerin Cosford <kerin.cosford@digital.justice.gov.uk>
2017-01-30 13:56:16 +0000

    Handling b64 encoding in Jenkins until I can figure out what the hell is going on

--- jenkinsfiles/create_rstudio_config.groovy
+++ jenkinsfiles/create_rstudio_config.groovy
@@ -7,9 +7,9 @@ node {
         for f in k8s-templates/r-studio-config/*
         do
             cat \$f | sed \\
-                -e s/{{\\.ClientSecretB64}}/"\$(echo -n ${env.CLIENT_SECRET}|base64)"/g \\
-                -e s/{{\\.ClientIDB64}}/"\$(echo -n ${env.CLIENT_ID}|base64)"/g \\
-                -e s/{{\\.Domain}}/"\$(echo -n ${env.DOMAIN}|base64)"/g | \\
+                -e s/{{\\.ClientSecretB64}}/${env.CLIENT_SECRET}/g \\
+                -e s/{{\\.ClientIDB64}}/${env.CLIENT_ID}/g \\
+                -e s/{{\\.Domain}}/${env.DOMAIN}/g | \\
             kubectl apply -f -
         done
         """
