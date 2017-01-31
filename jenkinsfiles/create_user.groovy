24a68cb23f61cff193ff8f48ef08464a933e931b
Kerin Cosford <kerin.cosford@digital.justice.gov.uk>
2017-01-31 13:29:28 +0000

    Fixed create user Jenkinsfile

--- jenkinsfiles/create_user.groovy
+++ jenkinsfiles/create_user.groovy
@@ -9,8 +9,8 @@ node {
         for f in k8s-templates/user-base/*
         do
             cat \$f | sed \\
-                -e 's/{{\\.Username}}/\${USERNAME}/g' \\
-                -e 's/{{\\.EFSHostname}}/${env.EFS_HOSTNAME}/g' | \\
+                -e s/{{\\.Username}}/\$USERNAME/g \\
+                -e s/{{\\.EFSHostname}}/${env.EFS_HOSTNAME}/g | \\
             kubectl apply -f -
         done
         """
