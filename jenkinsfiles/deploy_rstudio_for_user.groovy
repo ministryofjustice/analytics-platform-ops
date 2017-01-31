574e2a93315fefd44d5d13e4f62196860a43e662
Kerin Cosford <kerin.cosford@digital.justice.gov.uk>
2017-01-31 15:37:13 +0000

    Force lowercase username

--- jenkinsfiles/deploy_rstudio_for_user.groovy
+++ jenkinsfiles/deploy_rstudio_for_user.groovy
@@ -6,11 +6,12 @@ node {
         sh """
         CALLBACK_URL=\$(echo -n "https://${env.USERNAME}.r-studio.users.analytics.kops.integration.dsd.io/callback"|base64 -w 0)
         COOKIE_SECRET=\$(echo -n "${env.COOKIE_SECRET}"|base64 -w 0)
+        USERNAME=\$(echo '${env.USERNAME}' | tr '[:upper:]' '[:lower:]')
 
         for f in k8s-templates/r-studio-user/*
         do
             cat \$f | sed \\
-                -e s/{{\\.Username}}/${env.USERNAME}/g \\
+                -e s/{{\\.Username}}/\${USERNAME}/g \\
                 -e s/{{\\.CallbackURLB64}}/\${CALLBACK_URL}/g \\
                 -e s/{{\\.CookieSecretB64}}/\${COOKIE_SECRET}/g | \\
             kubectl apply -f -
