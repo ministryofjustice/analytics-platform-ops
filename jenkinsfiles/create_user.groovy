d7d50ada89d57895d5b6290adf8becb969566098
Kerin Cosford <kerin.cosford@digital.justice.gov.uk>
2017-02-02 17:03:55 +0000

    Parameterized user namespace ResourceQuota

--- jenkinsfiles/create_user.groovy
+++ jenkinsfiles/create_user.groovy
@@ -8,17 +8,21 @@ node {
 
         for f in k8s-templates/user-base/default-namespace/*
         do
-            cat \$f \
-            | sed \
-                -e s/{{\\.Username}}/\$USERNAME/g \
-                -e s/{{\\.EFSHostname}}/${env.EFS_HOSTNAME}/g \
+            cat \$f \\
+            | sed \\
+                -e s/{{\\.Username}}/\$USERNAME/g \\
+                -e s/{{\\.EFSHostname}}/${env.EFS_HOSTNAME}/g \\
             | kubectl apply -f -
         done
 
         for f in k8s-templates/user-base/user-namespace/*
         do
-            cat \$f \
-            | sed -e s/{{\\.Username}}/\$USERNAME/g \
+            cat \$f \\
+            | sed \\
+                -e s/{{\\.RequestCPU}}/${env.REQUEST_CPU}/g \\
+                -e s/{{\\.RequestMemory}}/${env.REQUEST_MEMORY}/g \\
+                -e s/{{\\.LimitCPU}}/${env.LIMIT_CPU}/g \\
+                -e s/{{\\.LimitMemory}}/${LIMIT_MEMORY}/g \\
             | kubectl apply -n user-\$USERNAME -f -
         done
         """
