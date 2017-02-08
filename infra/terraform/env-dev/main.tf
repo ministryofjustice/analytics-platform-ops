d709060084686480dc2e083e41ac065cca35e170
Kerin Cosford <kerin.cosford@digital.justice.gov.uk>
2017-02-08 14:34:03 +0000

    Added cluster_dns and data_buckets Terraform modules to dev env

--- infra/terraform/env-dev/main.tf
+++ infra/terraform/env-dev/main.tf
@@ -18,3 +18,18 @@ module "aws_vpc" {
     name = "${var.env}.${data.terraform_remote_state.base.xyz_root_domain}"
     cidr = "${var.vpc_cidr}"
 }
+
+module "cluster_dns" {
+    source = "../modules/cluster_dns"
+
+    env = "${var.env}"
+    root_zone_name = "${data.terraform_remote_state.base.xyz_dns_zone_name}"
+    root_zone_domain = "${data.terraform_remote_state.base.xyz_root_domain}"
+    root_zone_id = "${data.terraform_remote_state.base.xyz_dns_zone_id}"
+}
+
+module "data_buckets" {
+    source = "../modules/data_buckets"
+
+    env = "${var.env}"
+}
