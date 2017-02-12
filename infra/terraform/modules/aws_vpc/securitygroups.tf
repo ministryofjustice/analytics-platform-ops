c963003d10b80f053442ad93f55392e28f6321d6
Kerin Cosford <cosford@gmail.com>
2017-02-12 14:03:13 +0000

    Added extra master security group with node->master ingress rule to work around Heapster/cAdvisor port not being open to worker nodes

---
+++ infra/terraform/modules/aws_vpc/securitygroups.tf
@@ -0,0 +1,29 @@
+resource "aws_security_group" "node_extra" {
+    name = "node_extra"
+    description = "Extra non-Kops-managed node SG to use as target for other SG rules"
+    vpc_id = "${aws_vpc.main.id}"
+
+    tags {
+        Name = "node-extra.${var.name}"
+        Cluster = "${var.name}"
+    }
+}
+
+resource "aws_security_group" "master_extra" {
+    name = "master_extra"
+    description = "Extra non-Kops-managed master SG to use as target for other SG rules"
+    vpc_id = "${aws_vpc.main.id}"
+
+    tags {
+        Name = "master-extra.${var.name}"
+        Cluster = "${var.name}"
+    }
+
+    # Heapster / cAdvisor port access from worker nodes
+    ingress {
+        from_port = 10255
+        to_port = 10255
+        protocol = "tcp"
+        security_groups = ["${aws_security_group.node_extra.id}"]
+    }
+}
