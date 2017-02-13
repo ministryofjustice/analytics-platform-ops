da2aa0f0c125dbb7218d867ef4b12af5a090f0c1
Kerin Cosford <kerin.cosford@digital.justice.gov.uk>
2017-02-13 11:10:32 +0000

    Added ToolsDomain var to RStudio ingress and Jenkins job

--- jenkinsfiles/deploy_rstudio_for_user.groovy
+++ jenkinsfiles/deploy_rstudio_for_user.groovy
@@ -12,10 +12,11 @@ node {
     stage ("Deploy RStudio") {
         sh """
         USERNAME=\$(echo '${env.USERNAME}' | tr '[:upper:]' '[:lower:]')
+        TOOLS_DOMAIN=${env.TOOLS_DOMAIN}
         CLIENT_SECRET=\$(echo -n "${env.CLIENT_SECRET}"|base64 -w 0)
         CLIENT_ID=\$(echo -n "${env.CLIENT_ID}"|base64 -w 0)
         DOMAIN=\$(echo -n "${env.DOMAIN}"|base64 -w 0)
-        CALLBACK_URL=\$(echo -n "https://\${USERNAME}.rstudio.${env.TOOLS_DOMAIN}"|base64 -w 0)
+        CALLBACK_URL=\$(echo -n "https://\${USERNAME}.rstudio.${TOOLS_DOMAIN}"|base64 -w 0)
         COOKIE_SECRET=\$(echo -n "${env.COOKIE_SECRET}"|base64 -w 0)
         AWS_ACCESS_KEY_ID=\$(echo -n "${env.AWS_ACCESS_KEY_ID}"|base64 -w 0)
         AWS_SECRET_ACCESS_KEY=\$(echo -n "${AWS_SECRET_ACCESS_KEY}"|base64 -w 0)
@@ -25,6 +26,7 @@ node {
             cat \$f \\
             | sed \\
                 -e s/{{\\.Username}}/\${USERNAME}/g \\
+                -e s/{{\\.ToolsDOmain}}/\${TOOLS_DOMAIN}/g \\
                 -e s/{{\\.ClientSecretB64}}/\${CLIENT_SECRET}/g \\
                 -e s/{{\\.ClientIDB64}}/\${CLIENT_ID}/g \\
                 -e s/{{\\.DomainB64}}/\${DOMAIN}/g \\
