a56baed66f24b53b7a1296d0070872fb3ab08d4d
Kerin Cosford <kerin.cosford@digital.justice.gov.uk>
2017-02-21 17:20:30 +0000

    Added Elasticsearch cluster to dev env

---
+++ infra/terraform/modules/logging_elasticsearch/main.tf
@@ -0,0 +1,52 @@
+variable "name" {}
+variable "domain_name" {}
+variable "vpc_cidr" {}
+
+variable "es_version" {
+    default = "5.1"
+}
+
+variable "instance_type" {
+    default = "t2.small.elasticsearch"
+}
+
+variable "instance_count" {
+    default = 1
+}
+
+variable "dedicated_master_enabled" {
+    default = false
+}
+
+variable "dedicated_master_type" {
+    default = ""
+}
+
+variable "dedicated_master_count" {
+    default = 0
+}
+
+variable "ebs_enabled" {
+    default = true
+}
+
+variable "ebs_volume_size" {
+    default = 10
+}
+
+variable "ebs_volume_type" {
+    default = "gp2"
+}
+
+
+output "arn" {
+    value = "${aws_elasticsearch_domain.logging.arn}"
+}
+
+output "domain_id" {
+    value = "${aws_elasticsearch_domain.logging.domain_id}"
+}
+
+output "endpoint" {
+    value = "${aws_elasticsearch_domain.logging.endpoint}"
+}
