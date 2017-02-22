11efc06f68c25add2738992a1daf584aca8b7ff2
Aldo "xoen" Giambelluca <aldo.giambelluca@digital.justice.gov.uk>
2017-02-22 17:12:39 +0000

    Enforce S3 encryption on upload
    It may be annoying in the 'scratch' bucket so we may think alternative
    solutions later on (e.g. using AWS Lamda to encrypt object on upload).
    
    See: http://docs.aws.amazon.com/AmazonS3/latest/dev/UsingServerSideEncryption.html

--- infra/terraform/modules/data_buckets/s3.tf
+++ infra/terraform/modules/data_buckets/s3.tf
@@ -24,3 +24,75 @@ resource "aws_s3_bucket" "logs" {
         Name = "${var.env}-moj-analytics-logs"
     }
 }
+
+# Data in the 'source' bucket must be encrypted
+resource "aws_s3_bucket_policy" "source" {
+    bucket = "${aws_s3_bucket.source.id}"
+    policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "DenyIncorrectEncryptionHeaderInSource",
+      "Effect": "Deny",
+      "Action": "s3:PutObject",
+      "Resource": "${aws_s3_bucket.source.arn}/*",
+      "Principal": "*",
+      "Condition": {
+        "StringNotEquals": {
+          "s3:x-amz-server-side-encryption": "AES256"
+        }
+      }
+    },
+    {
+      "Sid": "DenyUnEncryptedObjectUploadsInSource",
+      "Effect": "Deny",
+      "Action": "s3:PutObject",
+      "Resource": "${aws_s3_bucket.source.arn}/*",
+      "Principal": "*",
+      "Condition": {
+        "Null": {
+          "s3:x-amz-server-side-encryption": true
+        }
+      }
+    }
+  ]
+}
+EOF
+}
+
+# Data in the 'scratch' bucket must be encrypted (at least for now)
+resource "aws_s3_bucket_policy" "scratch" {
+    bucket = "${aws_s3_bucket.scratch.id}"
+    policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "DenyIncorrectEncryptionHeaderInScratch",
+      "Effect": "Deny",
+      "Action": "s3:PutObject",
+      "Resource": "${aws_s3_bucket.scratch.arn}/*",
+      "Principal": "*",
+      "Condition": {
+        "StringNotEquals": {
+          "s3:x-amz-server-side-encryption": "AES256"
+        }
+      }
+    },
+    {
+      "Sid": "DenyUnEncryptedObjectUploadsInScratch",
+      "Effect": "Deny",
+      "Action": "s3:PutObject",
+      "Resource": "${aws_s3_bucket.scratch.arn}/*",
+      "Principal": "*",
+      "Condition": {
+        "Null": {
+          "s3:x-amz-server-side-encryption": true
+        }
+      }
+    }
+  ]
+}
+EOF
+}
