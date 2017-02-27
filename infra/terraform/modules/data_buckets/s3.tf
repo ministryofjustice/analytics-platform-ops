33f0afda973338582278d2732d7abb541de3fecf
Aldo "xoen" Giambelluca <aldo.giambelluca@digital.justice.gov.uk>
2017-02-27 10:37:14 +0000

    Don't force encryption in the 'scratch' S3 bucket
    This may be annoying for the user and there is a way of doing this by
    AWS Lambda.

--- infra/terraform/modules/data_buckets/s3.tf
+++ infra/terraform/modules/data_buckets/s3.tf
@@ -60,39 +60,3 @@ resource "aws_s3_bucket_policy" "source" {
 }
 EOF
 }
-
-# Data in the 'scratch' bucket must be encrypted (at least for now)
-resource "aws_s3_bucket_policy" "scratch" {
-    bucket = "${aws_s3_bucket.scratch.id}"
-    policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Sid": "DenyIncorrectEncryptionHeaderInScratch",
-      "Effect": "Deny",
-      "Action": "s3:PutObject",
-      "Resource": "${aws_s3_bucket.scratch.arn}/*",
-      "Principal": "*",
-      "Condition": {
-        "StringNotEquals": {
-          "s3:x-amz-server-side-encryption": "AES256"
-        }
-      }
-    },
-    {
-      "Sid": "DenyUnEncryptedObjectUploadsInScratch",
-      "Effect": "Deny",
-      "Action": "s3:PutObject",
-      "Resource": "${aws_s3_bucket.scratch.arn}/*",
-      "Principal": "*",
-      "Condition": {
-        "Null": {
-          "s3:x-amz-server-side-encryption": true
-        }
-      }
-    }
-  ]
-}
-EOF
-}
