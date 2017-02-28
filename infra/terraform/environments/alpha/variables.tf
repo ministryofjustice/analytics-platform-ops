e1863b39edb410cc0be25741e881a1f0ba66a892
Kerin Cosford <kerin.cosford@digital.justice.gov.uk>
2017-02-28 16:24:34 +0000

    Added "alpha" env terraform resources

---
+++ infra/terraform/environments/alpha/variables.tf
@@ -0,0 +1,11 @@
+variable "region" {
+    default = "eu-west-1"
+}
+
+variable "terraform_bucket_name" {}
+variable "terraform_base_state_file" {}
+variable "env" {}
+variable "vpc_cidr" {}
+variable "availability_zones" {
+    type = "list"
+}
