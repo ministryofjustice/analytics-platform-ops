fdd62d11a06fba12da6ac418f4ae24b2847f3f80
Kerin Cosford <kerin.cosford@digital.justice.gov.uk>
2017-03-08 10:51:32 +0000

    Updated rstudio deploy jenkinsfile to use Helm

---
+++ jenkinsfiles/deploy_rstudio_for_user.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -ex
+
+PLATFORM_ENV=$1
+USERNAME=$(echo $2 | tr '[:upper:]' '[:lower:]')
+
+# initialize Helm client
+helm init -c
+
+RELEASE_NAME=rstudio-${USERNAME}
+
+# Install if release isn't currently listed, otherwise upgrade
+if [[ -z "$(helm list ${RELEASE_NAME})" ]]; then
+    helm install charts/rstudio \
+        -f chart-env-config/${PLATFORM_ENV}/rstudio.yml \
+        --name ${RELEASE_NAME} \
+        --set Username=${USERNAME}
+else
+    helm upgrade ${RELEASE_NAME} charts/rstudio \
+        -f chart-env-config/${PLATFORM_ENV}/rstudio.yml \
+        --set Username=${USERNAME}
+fi
