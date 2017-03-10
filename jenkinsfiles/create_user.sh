e08fe4fab79887f3e2f486f2dfa21ea46cd365e1
Kerin Cosford <kerin.cosford@digital.justice.gov.uk>
2017-03-10 10:29:35 +0000

    Added --wait flag to Helm install/upgrade

--- jenkinsfiles/create_user.sh
+++ jenkinsfiles/create_user.sh
@@ -14,9 +14,11 @@ if [[ -z "$(helm list ${RELEASE_NAME})" ]]; then
     helm install charts/init-user \
         -f chart-env-config/${PLATFORM_ENV}/init-user.yml \
         --name ${RELEASE_NAME} \
-        --set Username=${USERNAME}
+        --set Username=${USERNAME} \
+        --wait
 else
     helm upgrade ${RELEASE_NAME} charts/init-user \
         -f chart-env-config/${PLATFORM_ENV}/init-user.yml \
-        --set Username=${USERNAME}
+        --set Username=${USERNAME} \
+        --wait
 fi
