81b0f395598fce6775329caa98e26e5853847a13
Aldo "xoen" Giambelluca <aldo.giambelluca@digital.justice.gov.uk>
2017-03-16 15:31:18 +0000

    init-user helm chart: Trim email/fullname
    Instead of doing it in the bash script

--- jenkinsfiles/create_user.sh
+++ jenkinsfiles/create_user.sh
@@ -6,7 +6,7 @@ set -ex
 
 PLATFORM_ENV=$1
 USERNAME=$(echo $2 | tr '[:upper:]' '[:lower:]')
-EMAIL=$(echo $3 | tr '[:upper:]' '[:lower:]')
+EMAIL=$3
 FULLNAME=$4
 
 # initialize Helm client
@@ -14,12 +14,10 @@ helm init -c
 
 RELEASE_NAME=init-user-${USERNAME}
 
-FULLNAME_PARAM=""
-
 # Install/upgrade the init-user helm chart
 helm upgrade ${RELEASE_NAME} charts/init-user \
     -f chart-env-config/${PLATFORM_ENV}/init-user.yml \
-    --set Username=${USERNAME} \
-    --set Email=${EMAIL} \
+    --set Username="$USERNAME" \
+    --set Email="$EMAIL" \
     --set Fullname="$FULLNAME" \
     --install --wait
