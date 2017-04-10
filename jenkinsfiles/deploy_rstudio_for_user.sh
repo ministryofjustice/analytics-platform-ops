38fc01c527e07f797928e3d2c34dc2e1b5a12f8b
Aldo "xoen" Giambelluca <aldo.giambelluca@digital.justice.gov.uk>
2017-04-10 16:48:25 +0100

    RStudio helm chart: Ability to pass AWS credentials from Jenkins

--- jenkinsfiles/deploy_rstudio_for_user.sh
+++ jenkinsfiles/deploy_rstudio_for_user.sh
@@ -3,6 +3,8 @@ set -ex
 
 PLATFORM_ENV=$1
 USERNAME=$(echo $2 | tr '[:upper:]' '[:lower:]')
+AWS_ACCESS_KEY_ID=$3
+AWS_SECRET_ACCESS_KEY=$4
 
 # initialize Helm client
 helm init -c
@@ -13,5 +15,7 @@ RELEASE_NAME=${USERNAME}-rstudio
 helm upgrade ${RELEASE_NAME} charts/rstudio \
     -f chart-env-config/${PLATFORM_ENV}/rstudio.yml \
     --set Username=${USERNAME} \
+    --set AWS.AccessKeyId=${AWS_ACCESS_KEY_ID} \
+    --set AWS.SecretAccessKey=${AWS_SECRET_ACCESS_KEY} \
     --namespace user-${USERNAME} \
     --install --wait
