87d9d790d890613630d5756124edc754639b1299
Kerin Cosford <kerin.cosford@digital.justice.gov.uk>
2017-04-13 11:44:25 +0100

    Added container_registry terraform module
    Contains IAM user with permissions to read/write/create ECR container repositories

---
+++ infra/terraform/modules/container_registry/iam.tf
@@ -0,0 +1,39 @@
+resource "aws_iam_user" "jenkinsci" {
+  name = "${var.env}-jenkinsci"
+}
+
+resource "aws_iam_access_key" "jenkinsci" {
+  user = "${aws_iam_user.jenkinsci.name}"
+}
+
+resource "aws_iam_user_policy" "jenkinsci" {
+  name = "${var.env}-jenkinsci"
+  user = "${aws_iam_user.jenkinsci.name}"
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ecr:GetAuthorizationToken",
+                "ecr:BatchCheckLayerAvailability",
+                "ecr:GetDownloadUrlForLayer",
+                "ecr:GetRepositoryPolicy",
+                "ecr:DescribeRepositories",
+                "ecr:ListImages",
+                "ecr:DescribeImages",
+                "ecr:BatchGetImage",
+                "ecr:InitiateLayerUpload",
+                "ecr:UploadLayerPart",
+                "ecr:CompleteLayerUpload",
+                "ecr:PutImage",
+                "ecr:CreateRepository"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+EOF
+}
