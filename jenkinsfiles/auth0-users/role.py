eadeb207018a633703926d8a9a3de1c54f0669fb
Aldo "xoen" Giambelluca <aldo.giambelluca@digital.justice.gov.uk>
2017-04-20 19:31:52 +0100

    create_auth0_user.py: Extracted some of the logic in classes

---
+++ jenkinsfiles/auth0-users/role.py
@@ -0,0 +1,60 @@
+import requests
+
+
+class Role(object):
+
+    def __init__(self, authz_api, authz_token, logger, role_data):
+        if not role_data:
+            raise ValueError("role_data can't be empty")
+
+        self.authz_api = authz_api
+        self.authz_token = authz_token
+        self.data = role_data
+        self.log = logger
+
+    def id(self):
+        return self.data["_id"]
+
+    def name(self):
+        return self.data["name"]
+
+    def description(self):
+        return self.data["description"]
+
+    def application_id(self):
+        return self.data["applicationId"]
+
+    def application_type(self):
+        return self.data["applicationType"]
+
+    def permission_ids(self):
+        return self.data["permissions"]
+
+
+    def add_permission(self, permission_id):
+        payload = {
+            'name': self.name(),
+            'description': self.description(),
+            'applicationId': self.application_id(),
+            'applicationType': self.application_type(),
+            'permissions': self.permission_ids(),
+        }
+        # Authorization Extension doesn't check for duplicated permissions
+        if permission_id not in payload['permissions']:
+            payload['permissions'].append(permission_id)
+
+        # Update the role
+        resp = requests.put(
+            '{}/roles/{}'.format(self.authz_api, self.id()),
+            headers={
+                "Authorization": "Bearer {}".format(self.authz_token)
+            },
+            json=payload,
+        )
+        if resp.status_code != 200:
+            self.log.error("Failed to add permission ({}) to role ({}): expected 200, got {}: {}".format(permission_id, self.id(), resp.status_code, resp.text))
+            return None
+
+        self.log.debug("Permission ({}) added to role ({})".format(permission_id, self.id()))
+        self.data = resp.json()
+        return self
---
+++ jenkinsfiles/auth0-users/role_api.py
@@ -0,0 +1,64 @@
+from role import Role
+
+import requests
+
+
+class RoleAPI(object):
+
+    def __init__(self, authz_api, authz_token, logger):
+        self.authz_api = authz_api
+        self.authz_token = authz_token
+        self.log = logger
+
+    def create(self, app_id, role_name):
+        role_data = self._get_role(app_id, role_name)
+        if not role_data:
+            role_data = self._create_role(role_name)
+
+        return Role(self.authz_api, self.authz_token, self.log, role_data)
+
+    def _get_all(self):
+        # Get existing roles
+        resp = requests.get(
+            '{}/roles'.format(self.authz_api),
+            headers={
+                "Authorization": "Bearer {}".format(self.authz_token)
+            }
+        )
+        if resp.status_code != 200:
+            self.log.error("Failed to get roles: expected 200, got {}: {}".format(resp.status_code, resp.text))
+            return None
+
+        return resp.json()['roles']
+
+
+    def _get_role(self, app_id, role_name):
+        roles = self._get_all()
+        for role in roles:
+            if role['applicationId'] == app_id and role['name'] == role_name:
+                return role
+        return None
+
+
+    def _create_role(self, app_id, role_name):
+        # Create new role
+        resp = requests.post(
+            '{}/roles'.format(self.authz_api),
+            headers={
+                "Authorization": "Bearer {}".format(self.authz_token),
+            },
+            json={
+              'name': role_name,
+              'description': role_name,
+              'applicationId': app_id,
+              'applicationType': "client",
+            }
+        )
+        if resp.status_code != 200:
+            self.log.error("Failed to create role: expected 200, got {}: {}".format(resp.status_code, resp.text))
+            return None
+
+        role = resp.json()
+
+        self.log.debug("Role created = {}".format(role))
+        return role
