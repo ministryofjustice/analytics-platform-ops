e59cb78c841b6d94951a0eacdf879003a7e38253
Aldo "xoen" Giambelluca <aldo.giambelluca@digital.justice.gov.uk>
2017-04-21 16:37:59 +0100

    create-auth0-user jenkins job: Passing Auth0 credentials vars
    See: [Jenkinsfile idiosynchrasies with escaping and quotes](https://gist.github.com/Faheetah/e11bd0315c34ed32e681616e41279ef4)

--- jenkinsfiles/create_auth0_user.groovy
+++ jenkinsfiles/create_auth0_user.groovy
@@ -12,6 +12,17 @@ node {
     // - adds this role to this group
     // - adds created user to this group
     stage ("Create Auth0 passwordless user") {
-        sh "jenkinsfiles/auth0-users/create_auth0_user.py ${env.AUTH0_DOMAIN} ${env.AUTH0_CLIENT_ID} ${env.AUTH0_CLIENT_SECRET} ${env.AUTHZ_API} ${env.APP_NAME} ${env.EMAIL}"
+      withCredentials([
+        [$class: 'StringBinding', credentialsId: 'AUTH0_DOMAIN', variable:
+'AUTH0_DOMAIN'],
+        [$class: 'StringBinding', credentialsId: 'AUTH0_CLIENT_ID', variable:
+'AUTH0_CLIENT_ID'],
+        [$class: 'StringBinding', credentialsId: 'AUTH0_CLIENT_SECRET', variable:
+'AUTH0_CLIENT_SECRET'],
+        [$class: 'StringBinding', credentialsId: 'AUTHZ_API', variable:
+'AUTHZ_API']
+      ]) {
+        sh "jenkinsfiles/auth0-users/create_auth0_user.py ${env.AUTH0_DOMAIN} ${env.AUTH0_CLIENT_ID} ${env.AUTH0_CLIENT_SECRET} ${env.AUTHZ_API} \\'${env.APP_NAME}\\' ${env.EMAIL}"
+      }
     }
 }
