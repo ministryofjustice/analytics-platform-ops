2a43bc0c516cfa79b40b11ef3158dd9f5c6ac9e5
Kerin Cosford <kerin.cosford@digital.justice.gov.uk>
2017-05-03 11:42:31 +0100

    Updated Terraform resources and Readme for new state backend in Terraform 0.9.x

--- infra/terraform/environments/dev/main.tf
+++ infra/terraform/environments/dev/main.tf
@@ -1,3 +1,11 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform.analytics.justice.gov.uk"
+    key    = "dev/terraform.tfstate"
+    region = "eu-west-1"
+  }
+}
+
 provider "aws" {
   region = "${var.region}"
 }
