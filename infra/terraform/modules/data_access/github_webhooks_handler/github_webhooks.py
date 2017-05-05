9450f92248f1c86b725536accb5a1ea9791a2ad1
Aldo "xoen" Giambelluca <aldo.giambelluca@digital.justice.gov.uk>
2017-05-05 17:34:04 +0100

    github_webhooks_handler lambda function: PR Amendments
    As per Andy's PR:
     - using full blown "github" name instead of the shorthand "gh"
       across the board (roles, permissions, etc...)
     - various improvements to the code style

---
+++ infra/terraform/modules/data_access/github_webhooks_handler/github_webhooks.py
@@ -0,0 +1,94 @@
+import hmac
+from hashlib import sha1
+import json
+import os
+
+import boto3
+from botocore.exceptions import ClientError
+
+
+SNS_ARN = os.environ["SNS_ARN"]
+STAGE = os.environ["STAGE"]
+GH_HOOK_SECRET = os.environ["GH_HOOK_SECRET"].encode("utf-8")
+
+sns = boto3.client("sns")
+
+
+class SNSTopicNotFound(Exception):
+    pass
+
+
+def publish_to_sns(event, context):
+    print("event received = {}".format(event))
+
+    signature = event["headers"]["X-Hub-Signature"]
+    message = event["body"]
+
+    if not valid_signature(signature, message):
+        return response(401, "Invalid signature")
+
+    try:
+        publish(
+            topic=topic_arn(event["headers"]["X-GitHub-Event"]),
+            message=message
+        )
+        return response(201, "event published to SNS")
+    except SNSTopicNotFound as error:
+        return response(404, "SNS topic '{}' not found".format(error))
+
+
+def topic_arn(event):
+    """
+    Returns the SNS Topic ARN for the event in the form of
+
+    "{sns_arn}:{stage}_gh_{event}_events", for example:
+    "arn:aws:sns:eu-west-1:1234:dev_github_team_events"
+    """
+    return "{sns_arn}:{stage}_github_{event}_events".format(
+        sns_arn=SNS_ARN,
+        stage=STAGE,
+        event=event,
+    )
+
+
+def publish(topic, message):
+    print("Publishing to '{}'. Message: '{}'".format(
+        topic,
+        message
+    ))
+
+    try:
+        sns.publish(
+            TopicArn=topic,
+            Message=message
+        )
+    except ClientError as e:
+        # Tried to publish to a non-existent SNS topic
+        if e.response["Error"]["Code"] == "NotFound":
+            raise SNSTopicNotFound(topic)
+
+
+def response(status_code, message):
+    print("Response {}: '{}'".format(status_code, message))
+
+    return {
+        "statusCode": status_code,
+        "headers": {"Content-Type": "application/json"},
+        "body": json.dumps({"message": message})
+    }
+
+
+# See GitHub documentation on webhooks' secret:
+# https://developer.github.com/v3/repos/hooks/
+def valid_signature(hmac_signature, message):
+    sha_name, signature = hmac_signature.split("=")
+    if sha_name == "sha1":
+        mac = hmac.new(
+            GH_HOOK_SECRET,
+            msg=message.encode("utf-8"),
+            digestmod=sha1
+        )
+        if hmac.compare_digest(mac.hexdigest().encode("utf-8"), signature.encode("utf-8")):
+            return True
+
+    return False
--- infra/terraform/modules/data_access/topics.tf
+++ infra/terraform/modules/data_access/topics.tf
@@ -3,13 +3,13 @@
 # Topic for GH membership events
 #
 # See: https://developer.github.com/v3/activity/events/types/#membershipevent
-resource "aws_sns_topic" "gh_membership_events" {
-  name = "${var.env}_gh_membership_events"
+resource "aws_sns_topic" "github_membership_events" {
+  name = "${var.env}_github_membership_events"
 }
 
 # Topic for GH team events
 #
 # See: https://developer.github.com/v3/activity/events/types/#teamevent
-resource "aws_sns_topic" "gh_team_events" {
-  name = "${var.env}_gh_team_events"
+resource "aws_sns_topic" "github_team_events" {
+  name = "${var.env}_github_team_events"
 }
