f11ebee79b2f93a1b66cb04a178d5cd8765b5492
Kerin Cosford <kerin.cosford@digital.justice.gov.uk>
2017-05-08 14:34:54 +0100

    Made SES IAM resource naming more explicit

--- infra/terraform/global/iam.tf
+++ infra/terraform/global/iam.tf
@@ -1,10 +1,10 @@
-resource "aws_iam_user" "auth0_ses_user" {
-    name = "auth0-ses-user"
+resource "aws_iam_user" "auth0_ses" {
+    name = "auth0_ses_user"
 }
 
-resource "aws_iam_user_policy" "auth0-ses-user" {
-    name = "auth0-ses-user"
-    user = "${aws_iam_user.auth0_ses_user.name}"
+resource "aws_iam_user_policy" "auth0_ses" {
+    name = "auth0_ses_user_policy"
+    user = "${aws_iam_user.auth0_ses.name}"
     policy = <<EOF
 {
   "Version": "2012-10-17",
@@ -22,6 +22,6 @@ resource "aws_iam_user_policy" "auth0-ses-user" {
 EOF
 }
 
-resource "aws_iam_access_key" "auth0_ses_user" {
-    user = "${aws_iam_user.auth0_ses_user.name}"
+resource "aws_iam_access_key" "auth0_ses" {
+    user = "${aws_iam_user.auth0_ses.name}"
 }
