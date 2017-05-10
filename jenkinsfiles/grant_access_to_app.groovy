932e1d9483eaeb73536d3c50f7235639d32e003b
Andy Driver <andy@pagezero.net>
2017-05-10 11:17:19 +0100

    Use correct Auth0 credentials

--- jenkinsfiles/grant_access_to_app.groovy
+++ jenkinsfiles/grant_access_to_app.groovy
@@ -20,17 +20,17 @@ node {
     // Creates passwordless user in Auth0 and adds to app group
     // NOTE: Groups provided by Auth0 Authorization Extension
     stage ("Create Auth0 passwordless user and add to app group") {
+      env.AUTHZ_API_ID = 'urn:auth0-authz-api'
       withCredentials([
-        usernamePassword(
-            credentialsId: 'auth0-mgmt-api',
-            usernameVariable: 'CLIENT_ID',
-            passwordVariable: 'CLIENT_SECRET'),
         string(
-            credentialsId: 'authz_api_url',
+            credentialsId: 'AUTH0_CLIENT_ID',
+            variable: 'CLIENT_ID'),
+        string(
+            credentialsId: 'AUTH0_CLIENT_SECRET',
+            variable: 'CLIENT_SECRET'),
+        string(
+            credentialsId: 'AUTHZ_API',
             variable: 'AUTHZ_API_URL'),
-        string(
-            credentialsId: 'authz_api_id',
-            variable: 'AUTHZ_API_ID'),
         string(
             credentialsId: 'AUTH0_DOMAIN',
             variable: 'AUTH0_DOMAIN')
