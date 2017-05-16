32f3cd850c62a5ad46064a23922debc5e6dbd68b
Aldo "xoen" Giambelluca <aldo.giambelluca@digital.justice.gov.uk>
2017-05-16 14:57:12 +0100

    Terraform: Renamed API Gateway stage => "api"
    As requested in PR, we previously used the environment name.

--- infra/terraform/environments/dev/outputs.tf
+++ infra/terraform/environments/dev/outputs.tf
@@ -3,5 +3,10 @@ output "env" {
 }
 
 output "github_webhooks_secret" {
-  value = "${var.gh_hook_secret}"
+    value = "${var.gh_hook_secret}"
+    sensitive = true
+}
+
+output "api_url" {
+    value = "${module.data_access.api_url}"
 }
