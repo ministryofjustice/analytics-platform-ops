26204800e7379ed8e2a4556df1705a249d2e83c7
Aldo "xoen" Giambelluca <aldo.giambelluca@digital.justice.gov.uk>
2017-05-16 14:57:12 +0100

    Terraform: Renamed API Gateway stage => "api"
    As requested in PR, we previously used the environment name.

--- infra/terraform/modules/data_access/api_gateway.tf
+++ infra/terraform/modules/data_access/api_gateway.tf
@@ -5,7 +5,7 @@ resource "aws_api_gateway_rest_api" "analytics" {
 # API Gateway stage (to give access to API from the outside)
 resource "aws_api_gateway_deployment" "analytics" {
   rest_api_id = "${aws_api_gateway_rest_api.analytics.id}"
-  stage_name  = "${var.env}"
+  stage_name  = "api"
   depends_on = ["aws_api_gateway_integration.post_github"]
 }
 
