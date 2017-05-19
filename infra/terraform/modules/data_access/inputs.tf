722511ffba8c6636c07b8c3ba470267bb30f1b30
Aldo "xoen" Giambelluca <aldo.giambelluca@digital.justice.gov.uk>
2017-05-19 15:40:23 +0100

    Lambda function which handles GitHub organization events
    This lambda function will be triggered by events in the SNS topic
    `${ENV}_github_organization_events`.
    
    Based on the [action in the GH event](https://developer.github.com/v3/activity/events/types/#organizationevent)
    will (asynchronously) invoke the `create_user_role()` or `delete_user_role`
    lambda function which will actually perform create/remove the IAM role
    for the user.
    
    NOTE: As the lambda functions are invoked asynchronously we don't get billed
    twice and we don't need to add more complexity by queuing these invocations
    manually.

--- infra/terraform/modules/data_access/inputs.tf
+++ infra/terraform/modules/data_access/inputs.tf
@@ -1,2 +1,4 @@
 variable "account_id" {}
 variable "env" {}
+
+variable "organization_events_topic_arn" {}
--- infra/terraform/modules/notifications/outputs.tf
+++ infra/terraform/modules/notifications/outputs.tf
@@ -1,3 +1,15 @@
 output "github_webhooks_handler_arn" {
   value = "${aws_lambda_function.github_webhooks_handler.arn}"
 }
+
+output "organization_events_topic_arn" {
+  value = "${aws_sns_topic.github_organization_events.arn}"
+}
+
+output "membership_events_topic_arn" {
+  value = "${aws_sns_topic.github_membership_events.arn}"
+}
+
+output "team_events_topic_arn" {
+  value = "${aws_sns_topic.github_team_events.arn}"
+}
