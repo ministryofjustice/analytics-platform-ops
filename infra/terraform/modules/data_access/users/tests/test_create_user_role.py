43d0b3b0cb4fb06ef48bb0dd85c06774c403d63c
Aldo "xoen" Giambelluca <aldo.giambelluca@digital.justice.gov.uk>
2017-05-24 12:05:37 +0100

    Tests for create_role/remove_role lambda functions
    Avoiding moto saved me times and makes things actually simpler/cleaner.
    Unfortunately these tests are using mocking hence have a bit too much
    knowledge of the implementation details of the tested code.
    
    I'm not sure how this can be avoided considering we don't want the tests
    to interact with the real AWS so we can't assert the side effects happened
    as we would normally do. On the bright side, the code tested is
    simple and we the boto3 library shouldn't change massively as it's the
    official AWS SDK so use mocking is less of a problem.

--- infra/terraform/modules/data_access/users/tests/test_create_user_role.py
+++ infra/terraform/modules/data_access/users/tests/test_create_user_role.py
@@ -1,5 +1,5 @@
-import boto3
-from botocore.exceptions import ClientError
+import json
+
 import pytest
 
 import users
@@ -9,11 +9,12 @@ import users
     "given_the_env_is_set",
     "given_iam_is_available",
 )
-def test_create_user_role_success(username, role_name):
+def test_create_user_role_success(iam_client_mock, username, role_name, trust_relationship):
     users.create_user_role({"username": username}, None)
 
-    try:
-        client = boto3.client("iam")
-        role = client.get_role(RoleName=role_name)
-    except Exception:
-        pytest.fail("Failed to read the role. Was it created?")
+    # Test role is created
+    iam_client_mock.create_role.assert_called_with(
+        RoleName=role_name,
+        Path="/users/",
+        AssumeRolePolicyDocument=json.dumps(trust_relationship),
+    )
