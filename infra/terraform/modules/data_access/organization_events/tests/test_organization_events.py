11d28617ce52d35b2ca6ba0ad8f763c21bdec212
Aldo "xoen" Giambelluca <aldo.giambelluca@digital.justice.gov.uk>
2017-05-25 13:56:33 +0100

    Amendments to as per Andy comments (PR #62)
    - Using `str#encode` instead of `bytes()`
    - Renamed environment variables to be explicit about the fact
      these ARNs belongs to lambda functions
    - Moved module documentation at the beginning of the file
    - Using `getLogger(__name__)` instead of `__package__`
    - Inline lambda functions payload instead of having it in a
      function
    - Removed some of the tests fixtures and used pytest's parametrize
      to remove some duplication
    
    See: https://github.com/ministryofjustice/analytics-platform-ops/pull/62#pullrequestreview-40250908

--- infra/terraform/modules/data_access/organization_events/tests/test_organization_events.py
+++ infra/terraform/modules/data_access/organization_events/tests/test_organization_events.py
@@ -5,31 +5,24 @@ import pytest
 import organization_events
 
 
+from tests.conftest import sns_event, TEST_PAYLOAD_BYTES, TEST_CREATE_ROLE_ARN, \
+    TEST_DELETE_ROLE_ARN
+
+
+@pytest.mark.parametrize("event,lambda_arn", [
+    (sns_event("member_added"), TEST_CREATE_ROLE_ARN),
+    (sns_event("member_removed"), TEST_DELETE_ROLE_ARN)
+])
 @pytest.mark.usefixtures(
     "given_the_env_is_set",
     "given_lambda_is_available",
 )
-def test_event_received_member_added(lambda_client_mock, member_added_event, create_role_arn, payload_bytes):
-    organization_events.event_received(member_added_event, None)
+def test_when_event_received_is_handled(lambda_client_mock, event, lambda_arn):
+    organization_events.event_received(event, None)
 
     # Test right lambda function is invoked asyncronously
     lambda_client_mock.invoke.assert_called_with(
-        FunctionName=create_role_arn,
-        Payload=payload_bytes,
-        InvocationType="Event",
-    )
-
-
-@pytest.mark.usefixtures(
-    "given_the_env_is_set",
-    "given_lambda_is_available",
-)
-def test_event_received_member_removed(lambda_client_mock, member_removed_event, delete_role_arn, payload_bytes):
-    organization_events.event_received(member_removed_event, None)
-
-    # Test right lambda function is invoked asyncronously
-    lambda_client_mock.invoke.assert_called_with(
-        FunctionName=delete_role_arn,
-        Payload=payload_bytes,
+        FunctionName=lambda_arn,
+        Payload=TEST_PAYLOAD_BYTES,
         InvocationType="Event",
     )
