4704e763fdd651eccbf5c6b2f6a6df70eebdf828
Aldo "xoen" Giambelluca <aldo.giambelluca@digital.justice.gov.uk>
2017-06-01 10:38:54 +0100

    Lambda function to create team bucket policies
    This lambda function will create a read-only policy and a read/write policy.
    
    The right policy will later be attached to the user role when he's added
    to the corresponding team:
     - read-only if 'member'
     - read/write if 'owner' or 'mainteiner'
    
    The read-only policy has the following permissions:
     - list buckets in AWS console
     - list objects in the team bucket
     - read objects in the team bucket
    
    The read/write policy has all the permissions in the read-only one plus:
     - can delete/rename/update/restore objects and their ACL in the team bucket
    
    NOTE: This lambda function is invoked asyncronously by the `team_event`
    lambda when a team is created (the lambda to create the team S3 bucket is
    also invoked at the same time).

--- infra/terraform/modules/data_access/teams/teams.py
+++ infra/terraform/modules/data_access/teams/teams.py
@@ -38,5 +38,96 @@ def create_team_bucket(event, context):
     )
 
 
+def create_team_bucket_policies(event, context):
+    """
+    Creates the policies for the team S3 bucket:
+      - a "readonly" policy and
+      - a "readwrite" policy
+
+    event = {"team": {"slug": "justice-league"}}
+    """
+    bucket = bucket_name(event["team"]["slug"])
+
+    create_policy(
+        name="{}-readonly".format(bucket),
+        bucket=bucket,
+        readwrite=False,
+    )
+
+    create_policy(
+        name="{}-readwrite".format(bucket),
+        bucket=bucket,
+        readwrite=True,
+    )
+
+
+def create_policy(name, bucket, readwrite):
+    LOG.debug("Creating '{}' policy".format(name))
+
+    policy_document = get_policy_document(bucket, readwrite)
+
+    client = boto3.client("iam")
+    client.create_policy(
+        PolicyName=name,
+        Path="/teams/",
+        PolicyDocument=json.dumps(policy_document),
+    )
+
+
+def get_policy_document(bucket, readwrite):
+    bucket_arn = "arn:aws:s3:::{}".format(bucket)
+
+    statements = [
+        {
+            "Sid": "ListBucketsInConsole",
+            "Effect": "Allow",
+            "Action": [
+                "s3:GetBucketLocation",
+                "s3:ListAllMyBuckets"
+            ],
+            "Resource": "arn:aws:s3:::*"
+        },
+        {
+            "Sid": "ListObjects",
+            "Action": [
+                "s3:ListBucket"
+            ],
+            "Effect": "Allow",
+            "Resource": [bucket_arn],
+        },
+        {
+            "Sid": "ReadObjects",
+            "Action": [
+                "s3:GetObject",
+                "s3:GetObjectAcl",
+                "s3:GetObjectVersion",
+            ],
+            "Effect": "Allow",
+            "Resource": "{}/*".format(bucket_arn)
+        },
+    ]
+
+    if readwrite:
+        statements.append(
+            {
+                "Sid": "UpdateRenameAndDeleteObjects",
+                "Action": [
+                    "s3:DeleteObject",
+                    "s3:DeleteObjectVersion",
+                    "s3:PutObject",
+                    "s3:PutObjectAcl",
+                    "s3:RestoreObject",
+                ],
+                "Effect": "Allow",
+                "Resource": "{}/*".format(bucket_arn)
+            }
+        )
+
+    return {
+        "Version": "2012-10-17",
+        "Statement": statements,
+    }
+
+
 def bucket_name(slug):
     return "{}-{}".format(os.environ["STAGE"], slug)
