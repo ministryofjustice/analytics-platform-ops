b87ef17453b855458a2d5930ee1f3e9e0cef78f0
Aldo "xoen" Giambelluca <aldo.giambelluca@digital.justice.gov.uk>
2017-06-01 10:38:54 +0100

    Lambda function to create team bucket policies
    This lambda function will create a read-only policy and a read/write policy.
    
    The right policy will later be attached to the user role when he's added
    to the corresponding team:
     - read-only if 'member'
     - read/write if 'owner' or 'mainteiner'
    
    The read-only policy has the following permissions:
     - list buckets in AWS console
     - list objects in the team bucket
     - read objects in the team bucket
    
    The read/write policy has all the permissions in the read-only one plus:
     - can delete/rename/update/restore objects and their ACL in the team bucket
    
    NOTE: This lambda function is invoked asyncronously by the `team_event`
    lambda when a team is created (the lambda to create the team S3 bucket is
    also invoked at the same time).

--- infra/terraform/modules/data_access/lambda_teams.tf
+++ infra/terraform/modules/data_access/lambda_teams.tf
@@ -65,3 +65,63 @@ resource "aws_iam_role_policy" "create_team_bucket" {
 }
 EOF
 }
+
+# Lambda function to create policies for a team S3 bucket
+resource "aws_lambda_function" "create_team_bucket_policies" {
+    description = "Creates policies for the team S3 bucket"
+    filename = "/tmp/teams.zip"
+    source_code_hash = "${data.archive_file.teams_zip.output_base64sha256}"
+    function_name = "${var.env}_create_team_bucket_policies"
+    role = "${aws_iam_role.create_team_bucket_policies.arn}"
+    handler = "teams.create_team_bucket_policies"
+    runtime = "python3.6"
+    timeout = 10
+    depends_on = ["data.archive_file.teams_zip"]
+    environment {
+        variables = {
+            STAGE = "${var.env}",
+        }
+    }
+}
+
+# Role assumed by the 'create_team_bucket_policies' lambda function
+resource "aws_iam_role" "create_team_bucket_policies" {
+    name = "${var.env}_create_team_bucket_policies"
+    assume_role_policy = "${data.aws_iam_policy_document.lambda_assume_role.json}"
+}
+
+# Policies for the 'create_team_bucket_policies' role
+resource "aws_iam_role_policy" "create_team_bucket_policies" {
+    name = "${var.env}_create_team_bucket_policies"
+    role = "${aws_iam_role.create_team_bucket_policies.id}"
+    policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "CanCreateIAMPolicies",
+      "Effect": "Allow",
+      "Action": [
+        "iam:CreatePolicy"
+      ],
+      "Resource": [
+        "arn:aws:iam::${var.account_id}:policy/teams/${var.env}-*"
+      ]
+    },
+    {
+      "Sid": "CanLog",
+      "Effect": "Allow",
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents",
+        "logs:DescribeLogStreams"
+      ],
+      "Resource": [
+        "arn:aws:logs:*:*:*"
+      ]
+    }
+  ]
+}
+EOF
+}
