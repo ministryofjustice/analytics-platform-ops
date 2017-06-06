0b05407afd975283017e066691120ac7c5885a97
Kerin Cosford <cosford@gmail.com>
2017-06-06 15:49:20 +0100

    Included env name in SAML metadata filename

--- infra/terraform/modules/federated_identity/iam.tf
+++ infra/terraform/modules/federated_identity/iam.tf
@@ -1,6 +1,6 @@
 resource "aws_iam_saml_provider" "auth0" {
   name = "${var.env}-auth0"
-  saml_metadata_document = "${file("${path.module}/saml/auth0-metadata.xml")}"
+  saml_metadata_document = "${file("${path.module}/saml/${var.env}-auth0-metadata.xml")}"
 }
 
 output "saml_provider_arn" {
