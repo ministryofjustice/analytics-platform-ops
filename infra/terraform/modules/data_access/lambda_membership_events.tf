a76bc28edf9c36836090f9a25e634e1fbf2e1f13
Aldo "xoen" Giambelluca <aldo.giambelluca@digital.justice.gov.uk>
2017-06-09 12:09:17 +0100

    Lambda function to attach team bucket IAM policy to user role
    This lambda function gets the team slug, the username and the policy
    type to attach (e.g. 'readonly', 'readwrite') in the following format:
    
    ```json
    {
          "user": {"username": "alice"},
          "team": {"slug": "justice-league"},
          "policy": {"type": "readwrite"},
      }
    ```
    
    It then attaches the corresponding IAM bucket policy for this team's S3 bucket
    to the user's IAM role.
    
    It raises an exception if the policy type is invalid.

--- infra/terraform/modules/data_access/lambda_membership_events.tf
+++ infra/terraform/modules/data_access/lambda_membership_events.tf
@@ -18,10 +18,9 @@ resource "aws_lambda_function" "membership_events" {
     depends_on = ["data.archive_file.membership_events_zip"]
     environment {
         variables = {
-            # TODO: Replace with actual lambda functions ARN
-            LAMBDA_ATTACH_BUCKET_POLICY_ARN = "TODO: attach_bucket_policy.arn",
+            LAMBDA_ATTACH_BUCKET_POLICY_ARN = "${aws_lambda_function.attach_bucket_policy.arn}",
+            # TODO: Replace with actual lambda function ARN
             LAMBDA_DETACH_BUCKET_POLICY_ARN = "TODO: detach_bucket_policy.arn",
-            # LAMBDA_ATTACH_BUCKET_POLICY_ARN = "${aws_lambda_function.attach_bucket_policy.arn}",
             # LAMBDA_DETACH_BUCKET_POLICY_ARN = "${aws_lambda_function.detach_bucket_policy.arn}",
         }
     }
@@ -53,22 +52,21 @@ resource "aws_iam_role" "membership_events_role" {
 resource "aws_iam_role_policy" "membership_events_role_policy" {
     name = "${var.env}_membership_events_role_policy"
     role = "${aws_iam_role.membership_events_role.id}"
-# TODO: Replace with actual lambda functions ARN
-    # {
-    #   "Sid": "CanInvokeLambdaFunctions",
-    #   "Effect": "Allow",
-    #   "Action": [
-    #     "lambda:InvokeFunction"
-    #   ],
-    #   "Resource": [
-    #     "${aws_lambda_function.attach_bucket_policy.arn}",
-    #     "${aws_lambda_function.detach_bucket_policy.arn}"
-    #   ]
-    # },
+# TODO: Add 'aws_lambda_function.detach_bucket_policy.arn'
     policy = <<EOF
 {
   "Version": "2012-10-17",
   "Statement": [
+    {
+      "Sid": "CanInvokeLambdaFunctions",
+      "Effect": "Allow",
+      "Action": [
+        "lambda:InvokeFunction"
+      ],
+      "Resource": [
+        "${aws_lambda_function.attach_bucket_policy.arn}"
+      ]
+    },
     {
       "Sid": "CanLog",
       "Effect": "Allow",
