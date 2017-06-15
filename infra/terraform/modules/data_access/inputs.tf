d2aadaaafc62329432485cae235e1011bad8a105
Aldo "xoen" Giambelluca <aldo.giambelluca@digital.justice.gov.uk>
2017-06-15 17:20:05 +0100

    Allow users' IAM roles to be assumed by EC2 instances
    This is to allow [kube2iam](https://github.com/jtblin/kube2iam#iam-roles) to
    attach these roles to the users' EC2 instances.

--- infra/terraform/modules/data_access/inputs.tf
+++ infra/terraform/modules/data_access/inputs.tf
@@ -3,6 +3,7 @@ variable "account_id" {}
 variable "env" {}
 
 variable "saml_provider_arn" {}
+variable "k8s_worker_role_arn" {}
 
 variable "membership_events_topic_arn" {}
 variable "organization_events_topic_arn" {}
