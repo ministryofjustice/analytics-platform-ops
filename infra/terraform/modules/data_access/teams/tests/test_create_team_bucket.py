e45372f1c8ed1846f1d72444389b767d37a76acc
Kerin Cosford <kerin.cosford@digital.justice.gov.uk>
2017-06-21 16:45:03 +0100

    Added S3 logging setup to create_team_bucket lambda function

--- infra/terraform/modules/data_access/teams/tests/test_create_team_bucket.py
+++ infra/terraform/modules/data_access/teams/tests/test_create_team_bucket.py
@@ -1,10 +1,10 @@
-import json
-
 import pytest
 
 import teams
 
-from tests.conftest import TEST_TEAM_SLUG, TEST_BUCKET_NAME, TEST_BUCKET_REGION
+from tests.conftest import (TEST_TEAM_SLUG, TEST_BUCKET_NAME,
+                            TEST_BUCKET_REGION, TEST_LOGS_BUCKET_NAME,
+                            TEST_LOGS_PREFIX)
 
 
 @pytest.mark.usefixtures(
@@ -19,5 +19,23 @@ def test_when_the_team_is_created_the_bucket_is_created(s3_client_mock):
         ACL="private",
         CreateBucketConfiguration={
             "LocationConstraint": TEST_BUCKET_REGION,
+        },
+    )
+
+
+@pytest.mark.usefixtures(
+    "given_the_env_is_set",
+    "given_s3_is_available",
+)
+def test_when_team_bucket_is_created_logging_is_enabled(s3_client_mock):
+    teams.create_team_bucket({"team": {"slug": TEST_TEAM_SLUG}}, None)
+
+    s3_client_mock.put_bucket_logging.assert_called_with(
+        Bucket=TEST_BUCKET_NAME,
+        BucketLoggingStatus={
+            'LoggingEnabled': {
+                'TargetBucket': TEST_LOGS_BUCKET_NAME,
+                'TargetPrefix': TEST_LOGS_PREFIX
+            }
         },
     )
