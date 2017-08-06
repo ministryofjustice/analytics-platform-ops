4566e89d70186248f40368b7e3df379f8ef82656
Kerin Cosford <cosford@gmail.com>
2017-08-06 19:08:26 +0100

    Updated RStudio upgrade utility script for new repo layout, and to handle users with minus symbols in their usernames

--- scripts/upgrade_rstudio_pods.sh
+++ scripts/upgrade_rstudio_pods.sh
@@ -5,11 +5,11 @@ DOCKER_TAG=${1:-"latest"}
 
 env=$(kubectl config current-context | cut -f 1 -d .)
 
-for user in $(kubectl get ns | grep user- | cut -f 1 -d ' ' | cut -f 2 -d -); do
+for user in $(kubectl get ns | grep user- | cut -f 1 -d ' ' | sed 's/^user-//'); do
     namespace=user-$user
 
-    helm upgrade $user-rstudio charts/rstudio \
-        -f chart-env-config/$env/rstudio.yml \
+    helm upgrade $user-rstudio mojanalytics/rstudio \
+        -f ../analytics-platform-config/chart-env-config/$env/rstudio.yml \
         --set Username=$user \
         --set AWS.IAMRole=${env}_user_${user} \
         --set RStudio.Image.Tag=$DOCKER_TAG \
