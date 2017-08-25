68643c9470d071bc04d8d2d707c4946dc9b5a145
Andy Driver <andyhd@users.noreply.github.com>
2017-08-25 15:42:33 +0100

    Set up the control panel RDS database (#105)
    * Add control-panel module
    
    * Added vpc ID, subnet IDs and SG IDs variables to control_panel_db TF module
    
    * Added cpanel RDS subnet and security group
    
    * Fixed cpanel DB identifier

---
+++ infra/terraform/modules/control_panel_db/resources.tf
@@ -0,0 +1,31 @@
+resource "aws_db_subnet_group" "control_panel_db" {
+  name       = "${var.env}_control_panel_db"
+  subnet_ids = ["${var.db_subnet_ids}"]
+}
+
+resource "aws_security_group" "control_panel_db" {
+  name = "${var.env}_control_panel_db"
+  vpc_id = "${var.vpc_id}"
+
+  ingress {
+    from_port = "5432"
+    to_port = "5432"
+    protocol = "tcp"
+    security_groups = ["${var.ingress_security_group_ids}"]
+  }
+}
+
+resource "aws_db_instance" "control_panel_db" {
+    identifier = "${var.env}-control-panel-db"
+    storage_type = "${var.storage_type}"
+    allocated_storage = "${var.allocated_storage}"
+    engine = "postgres"
+    engine_version = "9.6.2"
+    instance_class = "db.t2.micro"
+    name = "controlpanel"
+    username = "controlpanel"
+    password = "controlpanel"
+    db_subnet_group_name = "${aws_db_subnet_group.control_panel_db.name}"
+    vpc_security_group_ids = ["${aws_security_group.control_panel_db.*.id}"]
+    skip_final_snapshot = true
+}
