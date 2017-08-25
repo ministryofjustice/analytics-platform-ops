3ce76e3f096e36929124ee484152d58c80cd14f6
Kerin Cosford <kerin.cosford@digital.justice.gov.uk>
2017-08-25 11:36:06 +0100

    Added cpanel RDS subnet and security group

--- infra/terraform/modules/control_panel_db/resources.tf
+++ infra/terraform/modules/control_panel_db/resources.tf
@@ -1,4 +1,22 @@
+resource "aws_db_subnet_group" "control_panel_db" {
+  name       = "${var.env}_control_panel_db"
+  subnet_ids = ["${var.db_subnet_ids}"]
+}
+
+resource "aws_security_group" "control_panel_db" {
+  name = "${var.env}_control_panel_db"
+  vpc_id = "${var.vpc_id}"
+
+  ingress {
+    from_port = "5432"
+    to_port = "5432"
+    protocol = "tcp"
+    security_groups = ["${var.ingress_security_group_ids}"]
+  }
+}
+
 resource "aws_db_instance" "control_panel_db" {
+    identifier = "${var.env}_control_panel_db"
     storage_type = "${var.storage_type}"
     allocated_storage = "${var.allocated_storage}"
     engine = "postgres"
@@ -7,5 +25,7 @@ resource "aws_db_instance" "control_panel_db" {
     name = "controlpanel"
     username = "controlpanel"
     password = "controlpanel"
-    db_subnet_group_name = "${var.env}-control-panel-db"
+    db_subnet_group_name = "${aws_db_subnet_group.control_panel_db.name}"
+    vpc_security_group_ids = ["${aws_security_group.control_panel_db.*.id}"]
+    skip_final_snapshot = true
 }
