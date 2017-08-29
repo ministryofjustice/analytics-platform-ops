b12cfd1de1727b3907005c4d97f0e30dd441533f
Kerin Cosford <kerin.cosford@digital.justice.gov.uk>
2017-08-29 12:11:17 +0100

    Added kubernetes cluster Terraform module

---
+++ infra/terraform/modules/kubernetes_cluster/outputs.tf
@@ -0,0 +1,39 @@
+output "node_sg_id" {
+  value = "${aws_security_group.node.id}"
+}
+output "node_asg_name" {
+  value = "${aws_autoscaling_group.node.name}"
+}
+output "node_asg_id" {
+  value = "${aws_autoscaling_group.node.id}"
+}
+output "node_asg_arn" {
+  value = "${aws_autoscaling_group.node.arn}"
+}
+output "master_sg_id" {
+  value = "${aws_security_group.master.id}"
+}
+output "master_elb_sg_id" {
+  value = "${aws_security_group.master_elb.id}"
+}
+output "master_elb_dns_name" {
+  value = "${aws_elb.master.dns_name}"
+}
+output "master_internal_elb_dns_name" {
+  value = "${aws_elb.master_internal.dns_name}"
+}
+output "master_asg_name" {
+  value = "${aws_autoscaling_group.master.name}"
+}
+output "master_asg_id" {
+  value = "${aws_autoscaling_group.master.id}"
+}
+output "master_asg_arn" {
+  value = "${aws_autoscaling_group.master.arn}"
+}
+output "cloudwatch_log_group_name" {
+  value = "${aws_cloudwatch_log_group.cluster.name}"
+}
+output "cloudwatch_log_group_arn" {
+  value = "${aws_cloudwatch_log_group.cluster.arn}"
+}
