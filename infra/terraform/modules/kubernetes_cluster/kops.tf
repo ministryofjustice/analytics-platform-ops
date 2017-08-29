bd3b05acaa68d5bf36659d7da178ee18360183af
Kerin Cosford <kerin.cosford@digital.justice.gov.uk>
2017-08-29 12:11:17 +0100

    Added kubernetes cluster Terraform module

---
+++ infra/terraform/modules/kubernetes_cluster/kops.tf
@@ -0,0 +1,32 @@
+resource "null_resource" "check_kops_version" {
+  provisioner "local-exec" {
+    command = "${path.module}/bin/check_kops_version.sh ${path.module}/data/user_data.sh"
+  }
+}
+
+resource "null_resource" "wait_for_dns_resolution" {
+  provisioner "local-exec" {
+    command = "${path.module}/bin/wait_for_dns_resolution.sh ${var.cluster_fqdn}"
+  }
+}
+
+resource "null_resource" "create_cluster" {
+  depends_on = [
+    "null_resource.check_kops_version",
+    "null_resource.wait_for_dns_resolution"
+  ]
+  provisioner "local-exec" {
+    command = "kops create cluster --dns ${var.dns} --networking flannel --zones=${join(",", data.aws_availability_zones.available.names)} --node-count=${var.node_asg_desired} --master-zones=${data.template_file.master_azs.rendered} --target=terraform --api-loadbalancer-type=public --vpc=${var.vpc_id} --state=s3://${var.kops_s3_bucket_id} --kubernetes-version ${var.kubernetes_version} ${var.cluster_fqdn}"
+  }
+  provisioner "local-exec" {
+    when    = "destroy"
+    command = "kops delete cluster --yes --state=s3://${var.kops_s3_bucket_id} --unregister ${var.cluster_fqdn}"
+  }
+}
+
+resource "null_resource" "delete_tf_files" {
+  depends_on = [ "null_resource.create_cluster" ]
+  provisioner "local-exec" {
+    command = "rm -rf out"
+  }
+}
