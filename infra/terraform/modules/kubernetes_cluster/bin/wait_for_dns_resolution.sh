8a5ec7d7ff6e76a703ee427da9ffb1ae4ed64673
Kerin Cosford <kerin.cosford@digital.justice.gov.uk>
2017-08-30 16:30:03 +0100

    terraform module: updated dns resolution script to check for n number of successive successful checks

--- infra/terraform/modules/kubernetes_cluster/bin/wait_for_dns_resolution.sh
+++ infra/terraform/modules/kubernetes_cluster/bin/wait_for_dns_resolution.sh
@@ -4,13 +4,18 @@ set -eu -o pipefail
 
 DOMAIN=$1
 
-# During propagation DNS records can appear and disappear, so make sure
-# we get three positive hits before proceeding
+# During propagation DNS records can appear and disappear, so check
+# DNS resolves for a number of successive checks so we're really sure
 
 COUNTER=0
 while [[ $COUNTER -lt 10 ]]
 do
-    if [[ $(dig ns $DOMAIN +short | wc -l | awk '{print $1}') == "4" ]]
+    NUM_RECORDS=$(dig ns $DOMAIN +short | wc -l | awk '{print $1}')
+
+    if [[ $NUM_RECORDS == "0" ]]
+    then
+        COUNTER=0
+    elif [[ $NUM_RECORDS == "4" ]]
     then
         COUNTER=$((COUNTER+1))
     fi
