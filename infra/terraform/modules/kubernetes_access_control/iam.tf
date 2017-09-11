0e3287a5387a37bd2e5adb55b5e1136748aa46cc
Kerin Cosford <kerin.cosford@digital.justice.gov.uk>
2017-09-11 16:50:24 +0100

    k8s access control module: renamed node and master roles to match kops naming convention

--- infra/terraform/modules/kubernetes_access_control/iam.tf
+++ infra/terraform/modules/kubernetes_access_control/iam.tf
@@ -1,20 +1,20 @@
 resource "aws_iam_instance_profile" "kubernetes_masters" {
-  name = "kubernetes_masters"
+  name = "masters.${var.cluster_name}"
   role = "${aws_iam_role.kubernetes_masters.name}"
 }
 
 resource "aws_iam_instance_profile" "kubernetes_nodes" {
-  name = "kubernetes_nodes"
+  name = "nodes.${var.cluster_name}"
   role = "${aws_iam_role.kubernetes_nodes.name}"
 }
 
 resource "aws_iam_role" "kubernetes_masters" {
-  name               = "kubernetes_masters"
+  name               = "masters.${var.cluster_name}"
   assume_role_policy = "${data.aws_iam_policy_document.kubernetes_assume_role_policy.json}"
 }
 
 resource "aws_iam_role" "kubernetes_nodes" {
-  name               = "kubernetes_nodes"
+  name               = "nodes.${var.cluster_name}"
   assume_role_policy = "${data.aws_iam_policy_document.kubernetes_assume_role_policy.json}"
 }
 
@@ -36,13 +36,13 @@ resource "aws_iam_policy_attachment" "kubernetes_masters" {
 }
 
 resource "aws_iam_policy" "kubernetes_masters" {
-  name        = "kubernetes_masters"
+  name        = "masters.${var.cluster_name}"
   description = "Policy for Kubernetes master instances"
   policy      =  "${data.aws_iam_policy_document.kubernetes_masters_aws_iam_role_policy.json}"
 }
 
 resource "aws_iam_policy" "kubernetes_nodes_and_master" {
-  name        = "kubernetes_nodes_and_master"
+  name        = "nodes.${var.cluster_name}"
   description = "Policy for Kubernetes node and master instances"
   policy      = "${data.aws_iam_policy_document.kubernetes_nodes_aws_iam_role_policy.json}"
 }
