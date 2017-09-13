1735f46bfb136fc675c8e6d3b8c46ef521f700aa
Kerin Cosford <kerin.cosford@digital.justice.gov.uk>
2017-09-13 14:41:00 +0100

    k8s cluster module: updated AWS tags to match kops naming scheme, and renamed nodeup config ‘autoscaling group’ var name to ‘instance group’ to more accurately reflect what it represents

--- infra/terraform/modules/kubernetes_cluster/other_resources.tf
+++ infra/terraform/modules/kubernetes_cluster/other_resources.tf
@@ -1,4 +1,4 @@
 resource "aws_cloudwatch_log_group" "cluster" {
-  name              = "/kubernetes/cluster/${var.cluster_name}"
+  name              = "/kubernetes/cluster/${var.cluster_fqdn}"
   retention_in_days = "${var.cloudwatch_log_group_retention}"
 }
