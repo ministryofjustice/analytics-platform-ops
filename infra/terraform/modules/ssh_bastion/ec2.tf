500e3854ef492a80c33857f2eea39540fb0eb36c
Kerin Cosford <kerin.cosford@digital.justice.gov.uk>
2017-09-15 12:24:52 +0100

    ssh bastion module: only create ELB egress SG rule when we’re using an ELB

--- infra/terraform/modules/ssh_bastion/ec2.tf
+++ infra/terraform/modules/ssh_bastion/ec2.tf
@@ -30,6 +30,8 @@ resource "aws_security_group_rule" "elb_egress" {
   protocol          = "-1"
   cidr_blocks       = ["0.0.0.0/0"]
   ipv6_cidr_blocks  = ["::/0"]
+
+  count = "${var.use_elb}"
 }
 
 resource "aws_security_group" "bastion" {
