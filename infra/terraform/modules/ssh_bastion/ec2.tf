a81968ac1d4a3331401c5868691bf5db6b2f6b6d
Kerin Cosford <kerin.cosford@digital.justice.gov.uk>
2017-09-15 11:59:21 +0100

    ssh bastion module: add ELB egress rule

--- infra/terraform/modules/ssh_bastion/ec2.tf
+++ infra/terraform/modules/ssh_bastion/ec2.tf
@@ -21,6 +21,17 @@ resource "aws_security_group_rule" "elb_ssh_from_cidr" {
   count = "${var.use_elb}"
 }
 
+resource "aws_security_group_rule" "elb_egress" {
+  security_group_id = "${aws_security_group.elb.id}"
+  type = "egress"
+
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  ipv6_cidr_blocks  = ["::/0"]
+}
+
 resource "aws_security_group" "bastion" {
   name = "hosts-${var.name}"
   vpc_id = "${var.vpc_id}"
