0ee5cbc5e3aff7663287f20cf2b5bc6eac59b924
Kerin Cosford <kerin.cosford@digital.justice.gov.uk>
2017-09-15 14:19:16 +0100

    Added dev env example

---
+++ infra/terraform/environments/dev-env-example/data.tf
@@ -0,0 +1,10 @@
+data "aws_caller_identity" "current" {}
+
+data "terraform_remote_state" "base" {
+    backend = "s3"
+    config {
+        bucket = "${var.terraform_bucket_name}"
+        region = "${var.region}"
+        key = "${var.terraform_base_state_file}"
+    }
+}
