ca6e9e7a9afefacd2fc6f0410febb1ac4c60497c
Kerin Cosford <kerin.cosford@digital.justice.gov.uk>
2017-10-13 12:01:16 +0100

    nfs-backup module: Added versioning and object version transition rules

--- infra/terraform/modules/data_backup/s3.tf
+++ infra/terraform/modules/data_backup/s3.tf
@@ -7,18 +7,38 @@ resource "aws_s3_bucket" "nfs_backup" {
     Name = "${var.env}-moj-analytics-nfs-backup"
   }
 
+  versioning {
+    enabled = true
+  }
+
+  # Current object versions transition to Glacier after 30 days
+  # and are deleted after 90 (including deleted object markers)
+
+  # Old versions transition to Glacier after 14 days and are
+  # deleted after 30
+
   lifecycle_rule {
     id = "${var.env}-nfs-backup-transition"
     abort_incomplete_multipart_upload_days = 1
     enabled = true
 
     transition {
-      days          = "30"
+      days = 30
       storage_class = "GLACIER"
     }
 
     expiration {
-      days = "90"
+      days = 90
+      expired_object_delete_marker = true
+    }
+
+    noncurrent_version_transition {
+      days = 14
+      storage_class = "GLACIER"
+    }
+
+    noncurrent_version_expiration {
+      days = 30
     }
   }
 }
