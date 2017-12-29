42fc05158aa0de51cd6fc1ebcae27df5f5df3268
Kerin Cosford <cosford@gmail.com>
2017-12-29 16:04:53 +0000

    Added OIDC provider to terraform federated_identity module

--- infra/terraform/environments/dev/variables.tf
+++ infra/terraform/environments/dev/variables.tf
@@ -17,3 +17,12 @@ variable "softnas_ssh_public_key" {}
 
 variable "control_panel_api_db_username" {}
 variable "control_panel_api_db_password" {}
+
+# Auth0 tenant URLs MUST end with a trailing slash
+variable "oidc_provider_url" {}
+variable "oidc_client_ids" {
+  type = "list"
+}
+variable "oidc_provider_thumbprints" {
+  type = "list"
+}
