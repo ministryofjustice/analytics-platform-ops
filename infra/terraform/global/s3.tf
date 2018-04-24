6d44d12f0db8a92d4519e537e6e1566fd103f16e
Kerin Cosford <kerin.cosford@digital.justice.gov.uk>
2018-04-24 17:36:32 +0100

    Changed S3 upload bucket encryption to S3-managed
    Specifying KMS encryption resulted in system upload users resulted in AccessDenied errors on PutObject calls. Presumably users (or the bucket) require access to the KMS key, but documentation is hard to find - this should be revisited once this is clarified.

--- infra/terraform/global/s3.tf
+++ infra/terraform/global/s3.tf
@@ -34,11 +34,6 @@ resource "aws_s3_bucket" "terraform_state" {
     }
 }
 
-resource "aws_kms_key" "s3_uploads" {
-    description = "Key for upload object encryption"
-    deletion_window_in_days = 10
-}
-
 resource "aws_s3_bucket" "uploads" {
     bucket = "${var.uploads_bucket_name}"
     region = "${var.region}"
@@ -51,8 +46,7 @@ resource "aws_s3_bucket" "uploads" {
     server_side_encryption_configuration {
         rule {
             apply_server_side_encryption_by_default {
-                kms_master_key_id = "${aws_kms_key.s3_uploads.arn}"
-                sse_algorithm     = "aws:kms"
+                sse_algorithm     = "AES256"
             }
         }
     }
