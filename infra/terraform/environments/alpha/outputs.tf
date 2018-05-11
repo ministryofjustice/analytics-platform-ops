3a6b1c44c9a91287b739d2892161e5c4e86c0fb1
Aldo "xoen" Giambelluca <aldo.giambelluca@digital.justice.gov.uk>
2018-05-11 14:30:48 +0100

    Removed unused variables/outputs (related to lambdas)
    Variables deleted:
    - sentry_dsn
    - gh_hook_secret
    
    Outputs deleted:
    - github_webhooks_secret
    - webhooks_api_url

--- infra/terraform/environments/alpha/outputs.tf
+++ infra/terraform/environments/alpha/outputs.tf
@@ -9,12 +9,3 @@ output "control_panel_api_db_host" {
 output "control_panel_api_iam_role_name" {
     value = "${module.control_panel_api.iam_role_name}"
 }
-
-output "github_webhooks_secret" {
-    value = "${var.gh_hook_secret}"
-    sensitive = true
-}
-
-output "webhooks_api_url" {
-    value = "${module.webhooks_api.url}"
-}
