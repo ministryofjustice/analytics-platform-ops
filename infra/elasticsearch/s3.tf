3b1240c38264041f297e64d032ca68fc294f1a64
Mikael <mikellallison@gmail.com>
2018-06-29 16:00:07 +0100

    Elasticsearch: Backup
    Sets up AWS resources for an elasticsearch repository with an s3
    backend.

---
+++ infra/elasticsearch/s3.tf
@@ -0,0 +1,17 @@
+resource "aws_s3_bucket" "analytics_logging_bucket" {
+  bucket = "${var.name}"
+  acl    = "private"
+  region = "${var.region}"
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    "rule" {
+      "apply_server_side_encryption_by_default" {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
