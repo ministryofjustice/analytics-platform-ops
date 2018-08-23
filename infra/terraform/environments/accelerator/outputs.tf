4c3be1e16fdad66c9825c9fe29d3adbdc2ca82ec
David Read <david.read@hackneyworkshop.com>
2018-08-23 16:46:33 +0100

    Enabled the control panel db as it is needed.

--- infra/terraform/environments/accelerator/outputs.tf
+++ infra/terraform/environments/accelerator/outputs.tf
@@ -10,9 +10,9 @@ output "env" {
   value = "${var.env}"
 }
 
-# output "control_panel_api_db_host" {
-#   value = "${module.control_panel_api.db_host}"
-# }
+output "control_panel_api_db_host" {
+  value = "${module.control_panel_api.db_host}"
+}
 
 
 # output "control_panel_api_iam_role_name" {
