f246695b81f864f316d0d14446303be62f3ebdf8
Aldo "xoen" Giambelluca <aldo.giambelluca@digital.justice.gov.uk>
2018-09-12 12:43:17 +0100

    Reworked `ses_smtp_user` module as SMTP username is IAM Access Key ID
    I thought it would be the IAM user name. I was wrong.
    As the SMTP password is generated from the IAM Access Key Secret,
    it makes sense for the SMTP username to be the IAM Access Key ID.
    This means the SMTP user is really the IAM Access Key which can then
    be revoked if necessary.
    
    **NOTE**: As the SMTP password can only be used to send email if
    compromised and even if the user has other IAM permissions this will
    not be a disaster (that's why by design the SMTP password is not just
    the IAM Access Key Secret). Not that this user should really have
    more permissions than just the ability to send emails (Principle
    of Least Privilege)
    
    Also, in the IAM policy the `Resource` needs to be the SES email address
    identity ARN, not the SES domain identity ARN. This wasn't very clear
    from the documentation I've read but needs to change as well.

--- infra/terraform/modules/ses_smtp_user/outputs.tf
+++ infra/terraform/modules/ses_smtp_user/outputs.tf
@@ -1,3 +1,7 @@
-output "password" {
+output "smtp_username" {
+  value = "${aws_iam_access_key.access_key.id}"
+}
+
+output "smtp_password" {
   value = "${aws_iam_access_key.access_key.ses_smtp_password}"
 }
