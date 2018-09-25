17cbed8fa2fd00bf902a4ddff3d15010c7404434
Ravi Kotecha <kotecha.ravi@gmail.com>
2018-09-25 17:28:08 +0100

    WIP Script to update istio ingress
    This script has been updated to no longer mess with disabled ingresses

--- scripts/change_ingress_class.sh
+++ scripts/change_ingress_class.sh
@@ -1,16 +1,20 @@
 #!/usr/bin/env bash
 
-INGRESS_CLASS=${1:-"istio"}
-
 for ns in $(kubectl get ns --no-headers | cut -f 1 -d ' '); do
+    echo "working on ns: $ns"
     for ing in $(kubectl get ing --no-headers -n $ns --ignore-not-found=true| cut -f 1 -d ' '); do
         export annotations=$(kubectl get ing $ing -n $ns -o=jsonpath='{.metadata.annotations}')
-        echo " Updating: $ns/$ing $annotations"
-        #kubectl patch ing $ing -n $ns -p '{"metadata": {"annotations": {"kubernetes.io/ingress.class":"$INGRESS_CLASS"} } }'
-
-        # do we need this?
-        #kubectl patch ing $ing -n $ns -p '[{"op": "remove", "path": "/spec/tls"}]' --type=json
-
-        # TODO path
+        if [[ $annotations = *"nginx"* ]]; then
+            echo " 👌 Updating: $ns/$ing $annotations"
+            kubectl patch ing $ing -n $ns -p '{"metadata": {"annotations": {"kubernetes.io/ingress.class":"istio"} } }'
+            # do we need this? NO?
+            #kubectl patch ing $ing -n $ns -p '[{"op": "remove", "path": "/spec/tls"}]' --type=json
+            
+            # just delete's path from the first path in the ingress but that's ok
+            # because we only set one path
+            kubectl patch ing $ing -n $ns -p '[{"op": "remove", "path": "/spec/rules/0/http/paths/0/path"}]' --type=json
+        else
+            echo " ⛔ Skipping: $ns/$ing $annotations"
+        fi
     done
 done
