ce2923cd737198fa94a48f5ab0f52760dede2eb7
Ravi Kotecha <kotecha.ravi@gmail.com>
2018-10-01 13:44:14 +0100

    Invariant Checks Linter Fixes
    Since we have an .eslint config, I've updated the code to pass that lint check
    without warnings or errors.

--- scripts/invariant_checks/checks/missing-tls-block.js
+++ scripts/invariant_checks/checks/missing-tls-block.js
@@ -1,52 +1,48 @@
 function isMissingTlsBlock(ingress) {
-  return !ingress.spec.hasOwnProperty('tls')
+  return !Object.prototype.hasOwnProperty.call(ingress.spec, 'tls');
 }
 
 function formatInfo(ingress) {
-  return `${ingress.metadata.namespace} / ${ingress.metadata.name} - Missing TLS Block`
+  return `${ingress.metadata.namespace} / ${ingress.metadata.name} - Missing TLS Block`;
 }
 
 module.exports.check = async function check(client) {
   let passed = true;
-  let ingresses = await client.apis.extensions.v1beta1.namespaces('').ingresses.get()
-  for (let ingress of ingresses.body.items) {
-
+  const ingresses = await client.apis.extensions.v1beta1.namespaces('').ingresses.get();
+  for (const ingress of ingresses.body.items) {
     if (isMissingTlsBlock(ingress)) {
       passed = false;
       console.log(formatInfo(ingress));
     }
   }
   return passed;
-}
+};
 
 module.exports.fix = async function fix(client) {
-  let ingresses = await client.apis.extensions.v1beta1.namespaces('').ingresses.get()
-  let toFix = ingresses.body.items.filter(isMissingTlsBlock)
-  for (let ingress of toFix) {
-    let spec = ingress.spec;
-    let name = ingress.metadata.name;
-    let namespace = ingress.metadata.namespace;
-    let patch = {
+  const ingresses = await client.apis.extensions.v1beta1.namespaces('').ingresses.get();
+  const toFix = ingresses.body.items.filter(isMissingTlsBlock);
+  for (const ingress of toFix) {
+    const { spec, metadata: { namespace, name } } = ingress;
+    const patch = {
       body: {
         spec: {
           tls: [{
-            hosts: [spec.rules[0].host]
-          }]
-        }
-      }
-    }
+            hosts: [spec.rules[0].host],
+          }],
+        },
+      },
+    };
 
     try {
-      let updated = await client.apis.extensions.v1beta1.namespace(
-        namespace
+      // eslint-disable-next-line no-await-in-loop
+      await client.apis.extensions.v1beta1.namespace(
+        namespace,
       ).ingress(name).patch(
-        patch
-      )
+        patch,
+      );
     } catch (ex) {
       console.dir(ex, { depth: null, color: true });
       break;
     }
-
   }
-
-}
+};
