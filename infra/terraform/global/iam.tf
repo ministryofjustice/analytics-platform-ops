c4c7fe61af214b810fd84be34bfa5bef74245e18
Aldo "xoen" Giambelluca <aldo.giambelluca@digital.justice.gov.uk>
2018-10-02 12:04:04 +0100

    IAM user for Councourse pipeline which updates mojanalytics helm repo
    Councourse pipeline is here: https://github.com/ministryofjustice/analytics-platform-concourse-pipelines#update-helm-repoyaml
    
    The pipeline only needs to read and write from the S3 bucket used as
    Helm repository.
    
    This will obviously run only in an environment so I've put this IAM
    user in the global terraform file.

--- infra/terraform/global/iam.tf
+++ infra/terraform/global/iam.tf
@@ -100,3 +100,33 @@ resource "aws_iam_role_policy" "softnas" {
 }
 EOF
 }
+
+resource "aws_iam_user" "concourse_update_helm_repo" {
+  name = "concourse_update_helm_repo"
+}
+
+resource "aws_iam_access_key" "concourse_update_helm_repo_access_key" {
+  user = "${aws_iam_user.concourse_update_helm_repo.name}"
+}
+
+resource "aws_iam_user_policy" "concourse_update_helm_repo_policy" {
+  name = "${aws_iam_user.concourse_update_helm_repo.name}_policy"
+  user = "${aws_iam_user.concourse_update_helm_repo.name}"
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [{
+        "Sid": "UpdateHelmRepoS3Bucket",
+        "Effect": "Allow",
+        "Action": [
+            "s3:PutObject",
+            "s3:GetObject"
+        ],
+        "Resource": [
+            "arn:aws:s3:::${var.helm_repo_s3_bucket_name}/*"
+        ]
+    }]
+}
+EOF
+}
