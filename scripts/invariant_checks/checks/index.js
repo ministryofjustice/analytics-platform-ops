8dc5db6a721f05a2b92c3e857602f6f3508d7911
Ravi Kotecha <kotecha.ravi@gmail.com>
2018-10-09 12:45:35 +0100

    Invariant Check: Ghosted by the idler
    Add a check for deployments the idler disabled but didn't add to the unidler
    ingress, this can happens if the idler crashes half way through.
    
    It also takes a --fix flag to automatically fix the unidler ingress in the case
    of any failures.

--- scripts/invariant_checks/checks/index.js
+++ scripts/invariant_checks/checks/index.js
@@ -2,4 +2,5 @@
 module.exports = {
   'idled-deployments-with-more-than-0-replicas': require('./idled-deployments-with-more-than-0-replicas'),
   'missing-tls-block': require('./missing-tls-block'),
+  'ghosted-by-the-idler': require('./ghosted-by-the-idler'),
 };
