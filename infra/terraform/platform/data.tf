76940fa3e1e5decc1f2498db5a0c7a3e098f0db0
Kerin Cosford <kerin.cosford@digital.justice.gov.uk>
2018-12-04 17:02:38 +0000

    Updated TF modules in `platform` to use remote data sources from `platform-base`

--- infra/terraform/platform/data.tf
+++ infra/terraform/platform/data.tf
@@ -9,5 +9,15 @@ data "terraform_remote_state" "global" {
     key    = "${var.terraform_global_state_file}"
   }
 }
+
+data "terraform_remote_state" "platform_base" {
+  backend   = "s3"
+  workspace = "${terraform.workspace}"
+
+  config {
+    bucket               = "${var.terraform_bucket_name}"
+    region               = "${var.region}"
+    workspace_key_prefix = "platform-base:"
+    key                  = "terraform.tfstate"
   }
 }
