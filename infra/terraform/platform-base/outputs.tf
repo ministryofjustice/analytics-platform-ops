e749e9e741e3791eebfdf291c619a3d3049b810e
Kerin Cosford <kerin.cosford@digital.justice.gov.uk>
2018-12-04 16:05:27 +0000

    Add subnet ids to `platform-base` TF outputs

--- infra/terraform/platform-base/outputs.tf
+++ infra/terraform/platform-base/outputs.tf
@@ -18,6 +18,18 @@ output "vpc_cidr" {
   value = "${var.vpc_cidr}"
 }
 
+output "dmz_subnet_ids" {
+  value = "${module.aws_vpc.dmz_subnet_ids}"
+}
+
+output "private_subnet_ids" {
+  value = "${module.aws_vpc.private_subnet_ids}"
+}
+
+output "storage_subnet_ids" {
+  value = "${module.aws_vpc.storage_subnet_ids}"
+}
+
 output "availability_zones" {
   value = "${var.availability_zones}"
 }
