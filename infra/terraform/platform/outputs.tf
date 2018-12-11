ce2fcd36fd8aa6ca1ac6959111b4d5457b53e9c0
Kerin Cosford <kerin@users.noreply.github.com>
2018-12-11 12:20:20 +0000

    Split terraform resources into two resource sets (#237)
    * Move aws_vpc and cluster_dns modules into new terraform resource set / state object
    
    As some terraform resources make reference to IAM entities created by kops we now need to split the `platform` terraform into two resource sets, `platform-base` (run before kops) and `platform` (run after kops has provisioned a cluster).
    
    This commit moves the bare minimum terraform resources, namely the `aws_vpc` and `cluster_dns` modules into a new `platform-base` terraform resource set.
    
    * Move federated_identity module usage to platform-base
    
    Kops also requires OIDC info from federated_identity module output, so this commit moves that module to platform-base resources.
    
    * Use all 3 AZs in personal dev env
    
    * Add cluster config for `kerintest` environment
    
    * Rename ‘base’ state to ‘global’ in platform remote state data source
    
    * Add subnet ids to `platform-base` TF outputs
    
    * Updated TF modules in `platform` to use remote data sources from `platform-base`
    
    * Refactor `ec2_cluster_autoscaler_policy` TF module
    
    - `instance_role_name` now derived from TF workspace and global `platform_root_domain`
    - ASG ARN now obtained from an `aws_autoscaling_groups` TF data source
    
    * Fix unused and renamed output vars in platform TF
    
    * Rename `base` state to `global` in `platform-base` TF resources
    
    * Change `oidc_client_ids` to `oidc_client_id`, singular
    
    AWS OIDC Provider resources support multiple client IDs, but we only use one. As it’s useful to obtain that client ID from terraform for use by kops the `federated_identity` Terraform module has been changed to accept a single client ID, so an unambiguous single value can be obtained from Terraform outputs.
    
    * Use OIDC client ID from TF output instead of config YAML
    
    `platform-base` Terraform outputs contain the OIDC client ID used by both AWS Federation and Kubernetes, so the kops clusterspec script now uses that instead of a static value in kops config.yml
    
    * Update kops clusterspec to latest k8s 1.10.x release
    
    * Add k8s cluster tag to NAT gateways and EIPs
    
    kops/k8s requires tags on “shared” AWS resources, in the form `kubernetes.io/cluster/$clustername=shared` to prevent kops from modifying or deleting resources that it didn’t provision. This commit adds those missing tags to NAT gateways and elastic IPs
    
    * Split alpha tfvars into `platform` and `platform-base`
    
    * Split dev tfvars into `platform` and `platform-base`
    
    * Update README to reflect terraform `platform`/`platform-base` split

--- infra/terraform/platform/outputs.tf
+++ infra/terraform/platform/outputs.tf
@@ -14,20 +14,8 @@ output "terraform_bucket_name" {
   value = "${var.terraform_bucket_name}"
 }
 
-output "terraform_base_state_file" {
-  value = "${var.terraform_base_state_file}"
-}
-
-output "vpc_id" {
-  value = "${module.aws_vpc.vpc_id}"
-}
-
-output "vpc_cidr" {
-  value = "${var.vpc_cidr}"
-}
-
-output "availability_zones" {
-  value = "${var.availability_zones}"
+output "terraform_global_state_file" {
+  value = "${var.terraform_global_state_file}"
 }
 
 output "softnas_ssh_public_key" {
@@ -76,44 +64,4 @@ output "control_panel_api_db_password" {
 
 output "control_panel_api_db_host" {
   value = "${module.control_panel_api.db_host}"
-}
-
-output "oidc_provider_url" {
-  value = "${var.oidc_provider_url}"
-}
-
-output "oidc_client_ids" {
-  value = "${var.oidc_client_ids}"
-}
-
-output "oidc_provider_thumbprints" {
-  value = "${var.oidc_provider_thumbprints}"
-}
-
-output "dns_zone_domain" {
-  value = "${module.cluster_dns.dns_zone_domain}"
-}
-
-output "dns_zone_id" {
-  value = "${module.cluster_dns.dns_zone_id}"
-}
-
-output "dmz_subnets" {
-  value = "${module.aws_vpc.dmz_subnets}"
-}
-
-output "private_subnets" {
-  value = "${module.aws_vpc.private_subnets}"
-}
-
-output "extra_bastion_sg_id" {
-  value = "${module.aws_vpc.extra_bastion_sg_id}"
-}
-
-output "extra_master_sg_id" {
-  value = "${module.aws_vpc.extra_master_sg_id}"
-}
-
-output "extra_node_sg_id" {
-  value = "${module.aws_vpc.extra_node_sg_id}"
 }
