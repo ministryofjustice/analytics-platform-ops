c5dcb8387dffea4f2a4fe1b1ec31581a6a59668e
Kerin Cosford <kerin.cosford@digital.justice.gov.uk>
2018-12-12 17:07:19 +0000

    Add initial rough version of `kops_spec` TF module
    Currently a python script is used to generate kops Cluster and InstanceGroup specs from config YAML files and terraform outputs. This is somewhat clumsy and opaque, and requires extra tooling and steps during environment creation. This commit begins work on replacting that python script with terraform templates.

---
+++ infra/terraform/modules/kops_spec/main.tf
@@ -0,0 +1,62 @@
+data "template_file" "etcd_member" {
+  template = "${file("${path.module}/templates/etcd-member.snippet.tmpl")}"
+  count    = "${length(var.availability_zones)}"
+
+  vars {
+    availability_zone = "${element(var.availability_zones, count.index)}"
+  }
+}
+
+data "template_file" "public_subnet" {
+  template = "${file("${path.module}/templates/subnet.snippet.tmpl")}"
+
+  # Using AZs for count here instead of public_subnet_ids (or other subnet property)
+  # due to limitations on using length() with computed module outputs. See:
+  # https://github.com/hashicorp/terraform/issues/12570
+  count = "${length(var.availability_zones)}"
+
+  vars {
+    subnet_type              = "Utility"
+    subnet_name_prefix       = "dmz-"
+    subnet_id                = "${element(var.public_subnet_ids, count.index)}"
+    subnet_cidr_block        = "${element(var.public_subnet_cidr_blocks, count.index)}"
+    subnet_availability_zone = "${element(var.public_subnet_availability_zones, count.index)}"
+  }
+}
+
+data "template_file" "private_subnet" {
+  template = "${file("${path.module}/templates/subnet.snippet.tmpl")}"
+
+  # Using AZs for count here instead of private_subnet_ids (or other subnet property)
+  # due to limitations on using length() with computed module outputs. See:
+  # https://github.com/hashicorp/terraform/issues/12570
+  count = "${length(var.availability_zones)}"
+
+  vars {
+    subnet_type              = "Private"
+    subnet_name_prefix       = ""
+    subnet_id                = "${element(var.private_subnet_ids, count.index)}"
+    subnet_cidr_block        = "${element(var.private_subnet_cidr_blocks, count.index)}"
+    subnet_availability_zone = "${element(var.private_subnet_availability_zones, count.index)}"
+  }
+}
+
+resource "local_file" "kops" {
+  content  = "${data.template_file.kops.rendered}"
+  filename = "../../kops/${terraform.workspace}.yaml"
+}
+
+data "template_file" "kops" {
+  template = "${file("${path.module}/templates/kops.yaml.tmpl")}"
+
+  vars {
+    cluster_dns_name = "${var.cluster_dns_name}"
+    cluster_dns_zone = "${var.cluster_dns_zone}"
+
+    etcd_main_members   = "${join("", data.template_file.etcd_member.*.rendered)}"
+    etcd_events_members = "${join("", data.template_file.etcd_member.*.rendered)}"
+
+    public_subnets  = "${join("", data.template_file.public_subnet.*.rendered)}"
+    private_subnets = "${join("", data.template_file.private_subnet.*.rendered)}"
+  }
+}
---
+++ infra/terraform/modules/kops_spec/variables.tf
@@ -0,0 +1,30 @@
+variable "cluster_dns_name" {}
+variable "cluster_dns_zone" {}
+
+variable "availability_zones" {
+  type = "list"
+}
+
+variable "public_subnet_ids" {
+  type = "list"
+}
+
+variable "public_subnet_cidr_blocks" {
+  type = "list"
+}
+
+variable "public_subnet_availability_zones" {
+  type = "list"
+}
+
+variable "private_subnet_ids" {
+  type = "list"
+}
+
+variable "private_subnet_cidr_blocks" {
+  type = "list"
+}
+
+variable "private_subnet_availability_zones" {
+  type = "list"
+}
