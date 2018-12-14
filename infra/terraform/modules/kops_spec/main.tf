86df327d973784bb5a611d5e8596d46196f4047b
Kerin Cosford <kerin@users.noreply.github.com>
2018-12-14 12:23:08 +0000

    Replace kops cluster spec python script with Terraform equivalent (#238)
    * Add initial rough version of `kops_spec` TF module
    
    Currently a python script is used to generate kops Cluster and InstanceGroup specs from config YAML files and terraform outputs. This is somewhat clumsy and opaque, and requires extra tooling and steps during environment creation. This commit begins work on replacting that python script with terraform templates.
    
    * Add individual subnet outputs to aws_vpc tf module
    
    The current version of Terraform (0.11) makes it hard to work with maps of lists, which is how current subnet outputs are defined. This commit adds individual lists for subnet IDs, CIDRs and AZs.
    
    * Add kops_spec module to platform-base
    
    * Add master instancegroup to kops_spec TF module
    
    * Use subnet attributes to calculate kops resource counts
    
    * Correct instancegroup `role` template variable
    
    * Add node instancegroup to `kops_spec` TF module
    
    * Add highmem-nodes to `kops_spec` TF module
    
    * Add bastions instancegroup to `kops_spec` TF module
    
    * Format terraform HCL
    
    * Add missing vars to `kops_spec` TF module
    
    * Place `kops_spec` template vars in logical groupings
    
    * Add `root_volume_size` vars for kops instancegroups
    
    * Replace all `kops_spec` static args with variables
    
    * Add taints and nodeLabels to kops instancegroups
    
    * Replace kops_spec output file with terraform output
    
    * Format kops_spec terraform
    
    * Remove all kops resources
    
    As per-environment kops resources can now be generated with a single terraform command, none of the current kops files, scripts etc. are required
    
    * Update readme for terraform kops module
    
    Removes futzing around with python, yaml, temp files etc. and replaces it with piping terraform output into kops
    
    * Add workaround for terraform computed property count limitation
    
    * Update readme to detail required kops version

---
+++ infra/terraform/modules/kops_spec/main.tf
@@ -0,0 +1,143 @@
+# var.availability_zones is used for `count` in template_files rather than
+# a more directly appropriate property of subnets due to limitations on 
+# using length() with computed module outputs. See:
+# https://github.com/hashicorp/terraform/issues/12570
+
+data "template_file" "etcd_member" {
+  template = "${file("${path.module}/templates/etcd-member.snippet.tmpl")}"
+  count    = "${length(var.availability_zones)}"
+
+  vars {
+    availability_zone = "${element(var.availability_zones, count.index)}"
+  }
+}
+
+data "template_file" "public_subnet" {
+  template = "${file("${path.module}/templates/subnet.snippet.tmpl")}"
+  count    = "${length(var.availability_zones)}"
+
+  vars {
+    subnet_type              = "Utility"
+    subnet_name_prefix       = "dmz-"
+    subnet_id                = "${element(var.public_subnet_ids, count.index)}"
+    subnet_cidr_block        = "${element(var.public_subnet_cidr_blocks, count.index)}"
+    subnet_availability_zone = "${element(var.public_subnet_availability_zones, count.index)}"
+  }
+}
+
+data "template_file" "private_subnet" {
+  template = "${file("${path.module}/templates/subnet.snippet.tmpl")}"
+  count    = "${length(var.availability_zones)}"
+
+  vars {
+    subnet_type              = "Private"
+    subnet_name_prefix       = ""
+    subnet_id                = "${element(var.private_subnet_ids, count.index)}"
+    subnet_cidr_block        = "${element(var.private_subnet_cidr_blocks, count.index)}"
+    subnet_availability_zone = "${element(var.private_subnet_availability_zones, count.index)}"
+  }
+}
+
+data "template_file" "master_instancegroup" {
+  template = "${file("${path.module}/templates/instancegroup.snippet.tmpl")}"
+  count    = "${length(var.availability_zones)}"
+
+  vars {
+    role                      = "Master"
+    name                      = "master-${element(var.availability_zones, count.index)}"
+    cluster_dns_name          = "${var.cluster_dns_name}"
+    additional_security_group = "${var.masters_extra_sg_id}"
+    image                     = "${var.instancegroup_image}"
+    machine_type              = "${var.masters_machine_type}"
+    root_volume_size          = "${var.masters_root_volume_size}"
+    max_size                  = 1
+    min_size                  = 1
+    subnets                   = "  - ${element(var.availability_zones, count.index)}"
+    taints                    = "[]"
+    node_labels               = "{}"
+  }
+}
+
+data "template_file" "nodes_instancegroup" {
+  template = "${file("${path.module}/templates/instancegroup.snippet.tmpl")}"
+
+  vars {
+    role                      = "Node"
+    name                      = "nodes"
+    cluster_dns_name          = "${var.cluster_dns_name}"
+    additional_security_group = "${var.nodes_extra_sg_id}"
+    image                     = "${var.instancegroup_image}"
+    machine_type              = "${var.nodes_machine_type}"
+    root_volume_size          = "${var.nodes_root_volume_size}"
+    max_size                  = "${var.nodes_instancegroup_max_size}"
+    min_size                  = "${var.nodes_instancegroup_max_size}"
+    subnets                   = "  - ${join("\n  - ", var.private_subnet_availability_zones)}"
+    taints                    = "[]"
+    node_labels               = "{}"
+  }
+}
+
+data "template_file" "highmem_nodes_instancegroup" {
+  template = "${file("${path.module}/templates/instancegroup.snippet.tmpl")}"
+
+  vars {
+    role                      = "Node"
+    name                      = "highmem-nodes"
+    cluster_dns_name          = "${var.cluster_dns_name}"
+    additional_security_group = "${var.nodes_extra_sg_id}"
+    image                     = "${var.instancegroup_image}"
+    machine_type              = "${var.highmem_nodes_machine_type}"
+    root_volume_size          = "${var.highmem_nodes_root_volume_size}"
+    max_size                  = "${var.highmem_nodes_instancegroup_max_size}"
+    min_size                  = "${var.highmem_nodes_instancegroup_max_size}"
+    subnets                   = "  - ${join("\n  - ", var.private_subnet_availability_zones)}"
+    taints                    = "\n  - dedicated=highmem:NoSchedule"
+    node_labels               = "\n    node-role.kubernetes.io/highmem: \"\""
+  }
+}
+
+data "template_file" "bastions_instancegroup" {
+  template = "${file("${path.module}/templates/instancegroup.snippet.tmpl")}"
+
+  vars {
+    role                      = "Bastion"
+    name                      = "bastions"
+    cluster_dns_name          = "${var.cluster_dns_name}"
+    additional_security_group = "${var.bastions_extra_sg_id}"
+    image                     = "${var.instancegroup_image}"
+    machine_type              = "${var.bastions_machine_type}"
+    root_volume_size          = "${var.bastions_root_volume_size}"
+    max_size                  = "${var.bastions_instancegroup_max_size}"
+    min_size                  = "${var.bastions_instancegroup_max_size}"
+    subnets                   = "  - ${join("\n  - ", var.public_subnet_availability_zones)}"
+    taints                    = "[]"
+    node_labels               = "{}"
+  }
+}
+
+data "template_file" "kops" {
+  template = "${file("${path.module}/templates/kops.yaml.tmpl")}"
+
+  vars {
+    cluster_dns_name = "${var.cluster_dns_name}"
+    cluster_dns_zone = "${var.cluster_dns_zone}"
+
+    kops_state_bucket = "${var.kops_state_bucket}"
+    oidc_client_id    = "${var.oidc_client_id}"
+    oidc_issuer_url   = "${var.oidc_issuer_url}"
+    k8s_version       = "${var.k8s_version}"
+    vpc_id            = "${var.vpc_id}"
+    vpc_cidr          = "${var.vpc_cidr}"
+
+    etcd_main_members   = "${join("", data.template_file.etcd_member.*.rendered)}"
+    etcd_events_members = "${join("", data.template_file.etcd_member.*.rendered)}"
+
+    public_subnets  = "${join("", data.template_file.public_subnet.*.rendered)}"
+    private_subnets = "${join("", data.template_file.private_subnet.*.rendered)}"
+
+    master_instancegroups       = "${join("\n---\n", data.template_file.master_instancegroup.*.rendered)}"
+    nodes_instancegroup         = "${data.template_file.nodes_instancegroup.rendered}"
+    highmem_nodes_instancegroup = "${data.template_file.highmem_nodes_instancegroup.rendered}"
+    bastions_instancegroup      = "${data.template_file.bastions_instancegroup.rendered}"
+  }
+}
--- infra/terraform/platform-base/variables.tf
+++ infra/terraform/platform-base/variables.tf
@@ -27,3 +27,62 @@ variable "idp_saml_domain" {}
 variable "idp_saml_signon_url" {}
 variable "idp_saml_logout_url" {}
 variable "idp_saml_x509_cert" {}
+
+variable "k8s_version" {}
+variable "k8s_instancegroup_image" {}
+
+variable "k8s_masters_machine_type" {
+  default = "t2.medium"
+}
+
+variable "k8s_masters_root_volume_size" {
+  default = 64
+}
+
+variable "k8s_nodes_machine_type" {
+  default = "t2.medium"
+}
+
+variable "k8s_nodes_root_volume_size" {
+  default = 64
+}
+
+variable "k8s_nodes_instancegroup_min_size" {
+  default = 1
+}
+
+variable "k8s_nodes_instancegroup_max_size" {
+  default = 1
+}
+
+variable "k8s_highmem_nodes_machine_type" {
+  default = "t2.medium"
+}
+
+variable "k8s_highmem_nodes_root_volume_size" {
+  default = 64
+}
+
+variable "k8s_highmem_nodes_instancegroup_min_size" {
+  default = 0
+}
+
+variable "k8s_highmem_nodes_instancegroup_max_size" {
+  default = 0
+}
+
+variable "k8s_bastions_machine_type" {
+  default = "t2.micro"
+}
+
+variable "k8s_bastions_root_volume_size" {
+  default = 32
+}
+
+variable "k8s_bastions_instancegroup_min_size" {
+  default = 1
+}
+
+variable "k8s_bastions_instancegroup_max_size" {
+  default = 1
+}
