d53d92871f8ac0c72aff4a0adca36ec8604d4535
Kerin Cosford <cosford@gmail.com>
2018-12-18 16:41:10 +0000

    Add basic Atlantis deployment
    - uses Atlantis’s aws_vpc submodule instead of a separate VPC resource set due to issues with the atlantis terraform module always provisioning a VPC whether you like it or not
    - creates `global.mojanalytics.xyz` DNS zone

--- infra/terraform/global/atlantis.tf
+++ infra/terraform/global/atlantis.tf
@@ -1,57 +1,27 @@
-resource "aws_route53_zone" "global" {
-  name = "global.${var.platform_root_domain}."
-}
-
-resource "aws_route53_record" "global_ns" {
-  zone_id = "${aws_route53_zone.platform_zone.zone_id}"
-  name    = "${aws_route53_zone.global.name}"
-  type    = "NS"
-  ttl     = "30"
-
-  records = [
-    "${aws_route53_zone.global.name_servers.0}",
-    "${aws_route53_zone.global.name_servers.1}",
-    "${aws_route53_zone.global.name_servers.2}",
-    "${aws_route53_zone.global.name_servers.3}",
+# Workaround for the data.aws_route53_zone inside the
+# Atlantis module not calculating our aws_route53_zone.global
+# resource as a dependency at plan time
+data "aws_route53_zone" "global" {
+  name         = "${aws_route53_zone.global.name}"
+  depends_on = [
+    "aws_route53_zone.global",
+    "aws_route53_record.global_ns"
   ]
-}
-
-module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
-
-  name = "global"
-  cidr = "10.0.0.0/16"
-
-  azs             = ["${var.vpc_availability_zones}"]
-  private_subnets = ["${var.vpc_private_subnets_cidr_blocks}"]
-  public_subnets  = ["${var.vpc_public_subnets_cidr_blocks}"]
-
-  enable_nat_gateway = true
-
-  tags = {
-    Terraform = "true"
-    Environment = "global"
-  }
 }
 
 module "atlantis" {
   source = "terraform-aws-modules/atlantis/aws"
+  version = "1.5.1"
 
   name = "atlantis"
 
-  # VPC
-  cidr            = "${module.vpc.vpc_cidr_block}"
-  azs             = ["${var.vpc_availability_zones}"]
-  private_subnets = ["${module.vpc.private_subnets_cidr_blocks}"]
-  public_subnets  = ["${module.vpc.public_subnets_cidr_blocks}"]
+  cidr            = "${var.atlantis_vpc_cidr_block}"
+  azs             = ["${var.atlantis_vpc_availability_zones}"]
+  private_subnets = ["${var.atlantis_vpc_private_subnets_cidr_blocks}"]
+  public_subnets  = ["${var.atlantis_vpc_public_subnets_cidr_blocks}"]
 
-  # DNS (without trailing dot)
-  route53_zone_name = "${aws_route53_zone.global.name}"
+  route53_zone_name = "${data.aws_route53_zone.global.name}"
 
-  # ACM (SSL certificate) - Specify ARN of an existing certificate or new one will be created and validated using Route53 DNS
-//   certificate_arn = "arn:aws:acm:eu-west-1:135367859851:certificate/70e008e1-c0e1-4c7e-9670-7bb5bd4f5a84"
-
-  # Atlantis
   atlantis_github_user       = "mojanalytics"
   atlantis_github_user_token = "${var.atlantis_github_user_token}"
   atlantis_repo_whitelist    = ["github.com/ministryofjustice/analytics-platform-ops"]
