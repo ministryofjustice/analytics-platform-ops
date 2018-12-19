a3b626b4c22134cf6f021e0badbe1439893c7f75
Kerin Cosford <kerin@users.noreply.github.com>
2018-12-19 12:27:46 +0000

    Add Atlantis for Terraform CI (#243)
    * WIP: Add atlantis module to global TF
    
    * WIP: Add atlantis module to global TF
    
    * Add basic Atlantis deployment
    
    - uses Atlantis’s aws_vpc submodule instead of a separate VPC resource set due to issues with the atlantis terraform module always provisioning a VPC whether you like it or not
    - creates `global.mojanalytics.xyz` DNS zone
    
    * Configure Atlantis to use a test git repo

---
+++ infra/terraform/global/atlantis.tf
@@ -0,0 +1,39 @@
+# Workaround for the data.aws_route53_zone inside the
+# Atlantis module not calculating our aws_route53_zone.global
+# resource as a dependency at plan time
+data "aws_route53_zone" "global" {
+  name = "${aws_route53_zone.global.name}"
+
+  depends_on = [
+    "aws_route53_zone.global",
+    "aws_route53_record.global_ns",
+  ]
+}
+
+module "atlantis" {
+  source  = "terraform-aws-modules/atlantis/aws"
+  version = "1.5.1"
+
+  name = "atlantis"
+
+  cidr            = "${var.atlantis_vpc_cidr_block}"
+  azs             = ["${var.atlantis_vpc_availability_zones}"]
+  private_subnets = ["${var.atlantis_vpc_private_subnets_cidr_blocks}"]
+  public_subnets  = ["${var.atlantis_vpc_public_subnets_cidr_blocks}"]
+
+  route53_zone_name = "${data.aws_route53_zone.global.name}"
+
+  atlantis_github_user       = "mojanalytics"
+  atlantis_github_user_token = "${var.atlantis_github_user_token}"
+
+  atlantis_repo_whitelist = [
+    "github.com/ministryofjustice/analytics-platform-atlantis-example",
+  ]
+
+  github_organization = "ministryofjustice"
+  github_token        = "${var.atlantis_github_user_token}"
+
+  github_repo_names = [
+    "analytics-platform-atlantis-example",
+  ]
+}
--- infra/terraform/global/variables.tf
+++ infra/terraform/global/variables.tf
@@ -45,3 +45,30 @@ variable "prune_etcd_ebs_snapshot_env_vars" {
     DAYS_OLD           = "14"
   }
 }
+
+variable "atlantis_vpc_cidr_block" {
+  default = "10.0.0.0/16"
+}
+
+variable "atlantis_vpc_availability_zones" {
+  default = [
+    "eu-west-1a",
+    "eu-west-1b",
+  ]
+}
+
+variable "atlantis_vpc_private_subnets_cidr_blocks" {
+  default = [
+    "10.0.1.0/24",
+    "10.0.2.0/24",
+  ]
+}
+
+variable "atlantis_vpc_public_subnets_cidr_blocks" {
+  default = [
+    "10.0.101.0/24",
+    "10.0.102.0/24",
+  ]
+}
+
+variable "atlantis_github_user_token" {}
