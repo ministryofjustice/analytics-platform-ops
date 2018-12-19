4ed2cb9aa472a84a23841d5a143bd0ba82fd0511
Kerin Cosford <kerin@users.noreply.github.com>
2018-12-19 12:27:46 +0000

    Add Atlantis for Terraform CI (#243)
    * WIP: Add atlantis module to global TF
    
    * WIP: Add atlantis module to global TF
    
    * Add basic Atlantis deployment
    
    - uses Atlantis’s aws_vpc submodule instead of a separate VPC resource set due to issues with the atlantis terraform module always provisioning a VPC whether you like it or not
    - creates `global.mojanalytics.xyz` DNS zone
    
    * Configure Atlantis to use a test git repo

--- infra/terraform/global/dns.tf
+++ infra/terraform/global/dns.tf
@@ -2,3 +2,21 @@ resource "aws_route53_zone" "platform_zone" {
   name          = "${var.platform_root_domain}."
   force_destroy = "false"
 }
+
+resource "aws_route53_zone" "global" {
+  name = "global.${var.platform_root_domain}."
+}
+
+resource "aws_route53_record" "global_ns" {
+  zone_id = "${aws_route53_zone.platform_zone.zone_id}"
+  name    = "${aws_route53_zone.global.name}"
+  type    = "NS"
+  ttl     = "30"
+
+  records = [
+    "${aws_route53_zone.global.name_servers.0}",
+    "${aws_route53_zone.global.name_servers.1}",
+    "${aws_route53_zone.global.name_servers.2}",
+    "${aws_route53_zone.global.name_servers.3}",
+  ]
+}
