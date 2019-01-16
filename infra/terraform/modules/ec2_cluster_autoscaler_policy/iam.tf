4261eb743fb3b143acbf959788439e24360daf90
Mikael <mikellallison@gmail.com>
2019-01-16 14:36:12 +0000

    Scale Highmem to 0
    [Trello](https://trello.com/c/ivZAkPne)
    
    Relates to [PR](https://github.com/ministryofjustice/analytics-platform-config/pull/110)
    
    Small changes to module to:
    - Make it a little more explicit by adding another variable for ASGs
    - Allow for adding autoscaling groups that do not have similar name as the role the policy will be attached to.
    
    Also allowing Cluster autoscaler to manage the `highmem` autoscaling group

--- infra/terraform/modules/ec2_cluster_autoscaler_policy/iam.tf
+++ infra/terraform/modules/ec2_cluster_autoscaler_policy/iam.tf
@@ -18,7 +18,7 @@ data "aws_iam_policy_document" "policy" {
       "autoscaling:TerminateInstanceInAutoScalingGroup",
     ]
 
-    resources = ["${data.aws_autoscaling_groups.nodes.arns}"]
+    resources = ["${data.aws_autoscaling_groups.asgs.arns}"]
   }
 }
 
