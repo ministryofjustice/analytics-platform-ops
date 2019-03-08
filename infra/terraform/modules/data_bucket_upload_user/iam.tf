af899c1dea02ebf5b2addb1cb3e7d09c34165f2b
Josh Rowe <josh.rowe@digital.justice.gov.uk>
2019-03-08 11:31:17 +0000

    Add more bucket actions

--- infra/terraform/modules/data_bucket_upload_user/iam.tf
+++ infra/terraform/modules/data_bucket_upload_user/iam.tf
@@ -9,21 +9,45 @@ resource "aws_iam_access_key" "system_user" {
 
 data "aws_iam_policy_document" "system_user_s3_upload_readwrite" {
   statement {
+    sid = "ListUploadBucket"
+
     actions = [
-      "s3:ListMultipartUploadParts",
-      "s3:GetObject",
-      "s3:GetObjectAcl",
-      "s3:GetObjectVersion",
-      "s3:PutObject",
-      "s3:PutObjectAcl",
-      "s3:PutObjectVersion",
-      "s3:RestoreObject",
+      "s3:ListBucket",
     ]
 
     effect = "Allow"
 
     resources = [
-      "${var.upload_bucket_arn}/",
+      "${var.upload_bucket_arn}",
+      "${var.upload_bucket_arn}/*",
+    ]
+  }
+
+  statement {
+    sid = "ReadWriteUploadBucket"
+
+    actions = [
+      "s3:GetObject",
+      "s3:GetObjectAcl",
+      "s3:GetObjectVersion",
+      "s3:DeleteObject",
+      "s3:DeleteObjectVersion",
+      "s3:PutObject",
+      "s3:PutObjectAcl",
+      "s3:PutObjectVersion",
+      "s3:RestoreObject",
+      "s3:GetBucketLocation",
+      "s3:ListBucketMultipartUploads",
+      "s3:ListMultipartUploadParts",
+      "s3:AbortMultipartUpload",
+      "s3:CreateBucket",
+    ]
+
+    effect = "Allow"
+
+    resources = [
+      "${var.upload_bucket_arn}",
+      "${var.upload_bucket_arn}/*",
     ]
   }
 
@@ -78,19 +102,60 @@ resource "aws_iam_user_policy_attachment" "system_user_s3_upload" {
   policy_arn = "${aws_iam_policy.system_user_s3_readwrite.arn}"
 }
 
+resource "aws_iam_role" "lookups_job_role" {
+  name = "lookups_job_role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": ["glue.amazonaws.com", "ec2.amazonaws.com"]
+      },
+      "Effect": "Allow",
+      "Sid": "Assumejob"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "lookups_job_role_attatchment" {
+  policy_arn = "${aws_iam_policy.system_user_s3_readwrite.arn}"
+  role       = "${aws_iam_role.lookups_job_role.name}"
+}
+
 data "aws_iam_policy_document" "system_policy_s3_readonly" {
   statement {
+    sid = "ListUploadBucketRO"
+
     actions = [
-      "s3:GetObject",
-      "s3:GetObjectAcl",
-      "s3:GetObjectVersion",
       "s3:ListBucket",
     ]
 
     effect = "Allow"
 
     resources = [
-      "${var.upload_bucket_arn}/",
+      "${var.upload_bucket_arn}",
+    ]
+  }
+
+  statement {
+    sid = "ReadOnlyBucketRo"
+
+    actions = [
+      "s3:GetObject",
+      "s3:GetObjectAcl",
+      "s3:GetObjectVersion",
+    ]
+
+    effect = "Allow"
+
+    resources = [
+      "${var.upload_bucket_arn}",
+      "${var.upload_bucket_arn}/*",
     ]
   }
 
@@ -140,31 +205,6 @@ resource "aws_iam_policy" "system_user_s3_readonly" {
   policy = "${data.aws_iam_policy_document.system_policy_s3_readonly.json}"
 }
 
-resource "aws_iam_role" "lookups_job_role" {
-  name = "lookups_job_role"
-
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": ["glue.amazonaws.com", "ec2.amazonaws.com"]
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
-}
-EOF
-}
-
-resource "aws_iam_role_policy_attachment" "lookups_job_role_attatchment" {
-  policy_arn = "${aws_iam_policy.system_user_s3_readwrite.arn}"
-  role       = "${aws_iam_role.lookups_job_role.name}"
-}
-
 resource "aws_iam_role" "lookups_user_role" {
   name = "lookups_user_role"
 
@@ -178,7 +218,7 @@ resource "aws_iam_role" "lookups_user_role" {
         "Service": ["glue.amazonaws.com", "ec2.amazonaws.com"]
       },
       "Effect": "Allow",
-      "Sid": ""
+      "Sid": "AssumeUser"
     }
   ]
 }
