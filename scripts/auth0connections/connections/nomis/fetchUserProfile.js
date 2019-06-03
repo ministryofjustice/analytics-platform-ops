1383f33942a523e0eefdfdeef56eaa365d4b0eac
Ravi Kotecha <kotecha.ravi@gmail.com>
2019-06-03 14:47:07 +0100

    Auth0 additional connections
    Document how to create additional auth0 connections (without using teraform).
    This also adds a script to run, which will create the defined connections
    instead of documenting how to do this manually in the auth0 console.

---
+++ scripts/auth0connections/connections/nomis/fetchUserProfile.js
@@ -0,0 +1,34 @@
+function(accessToken, ctx, cb) {
+  var base_url = "{{gateway_url}}";
+  var user_endpoint = "{{user_endpoint}}";
+  var user_profile_url = base_url + user_endpoint;
+
+  // call oauth2 API with the accesstoken and create the profile
+  request.get(
+    user_profile_url,
+    {
+      headers: {
+        Authorization: "Bearer " + accessToken
+      }
+    },
+    function(err, resp, body) {
+      if (err) {
+        cb(err);
+        return;
+      }
+      if (!/^2/.test("" + resp.statusCode)) {
+        cb(body);
+        return;
+      }
+      var parsedBody = JSON.parse(body);
+      var profile = {
+        user_id: parsedBody.staffId,
+        nickname: parsedBody.name,
+        username: parsedBody.username,
+        blocked: !parsedBody.active,
+        activeCaseLoadId: parsedBody.activeCaseLoadId
+      };
+      cb(null, profile);
+    }
+  );
+}
