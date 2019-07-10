953df02fe33541547203edb0536fc82715fc85a9
David Read <david.read@hackneyworkshop.com>
2019-07-10 16:08:46 +0100

    Needs a wildcard to write files. This is how hmcts_upload_user is already configured manually.

--- infra/terraform/modules/data_upload_user/iam.tf
+++ infra/terraform/modules/data_upload_user/iam.tf
@@ -17,7 +17,7 @@ data "aws_iam_policy_document" "system_user_s3_upload_writeonly" {
     effect = "Allow"
 
     resources = [
-      "${var.upload_bucket_arn}/${var.org_name}/${var.system_name}/",
+      "${var.upload_bucket_arn}/${var.org_name}/${var.system_name}/*",
     ]
   }
 }
