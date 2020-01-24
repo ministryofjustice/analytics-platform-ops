60d8e7f9ce6ff1af7b364f0feae2b6560d305e43
Aldo "xoen" Giambelluca <aldo.giambelluca@digital.justice.gov.uk>
2020-01-24 16:56:17 +0000

    Added cloudwatch_alerts module to monitor SoftNAS
    Send email if CPU spikes over 80% for 2 five minutes periods in the last
    15 minutes.
    
    Unfortunately the `aws_sns_topic_subscription` Terraform resource
    doesn't support the `email` protocol:
    
    > Unsupported protocols include the following:
    > - email -- delivery of message via SMTP
    > - email-json -- delivery of JSON-encoded message via SMTP
    >
    > These are unsupported because the endpoint needs to be authorized and
    > does not generate an ARN until the target email address has been
    > validated. This breaks the Terraform model and as a result are not
    > currently supported.
    
    Apparently this is because of the manual click to subscribe required.
    
    See: https://www.terraform.io/docs/providers/aws/r/sns_topic_subscription.html
    
    For this reason the module creates a CloudFormation stack which creates
    SNS topic. The CloudWatch alarm then uses this SNS topic created by
    this stack to send the email notifications.
    
    Ticket: https://trello.com/c/wI7T79OV

---
+++ infra/terraform/modules/cloudwatch_alerts/cloudwatch.tf
@@ -0,0 +1,28 @@
+resource "aws_cloudwatch_metric_alarm" "cpu_threshold" {
+  count             = "${length(var.ec2_instance_names)}"
+  alarm_name        = "${var.name}_${element(var.ec2_instance_names, count.index)}-cpu-alarm"
+  alarm_description = "This metric monitors EC2 CPU utilisation"
+
+  dimensions = {
+    InstanceId = "${element(var.ec2_instance_ids, count.index)}"
+  }
+
+  namespace           = "AWS/EC2"
+  metric_name         = "CPUUtilization"
+  statistic           = "Maximum"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  threshold           = "${var.cpu_threshold}"
+  period              = "300"                           # 5 minutes
+  evaluation_periods  = "3"
+  datapoints_to_alarm = "2"
+  treat_missing_data  = "breaching"
+
+  actions_enabled = "true"
+  alarm_actions   = ["${ aws_cloudformation_stack.notifications.outputs["ARN"] }"]
+
+  tags = "${merge(map(
+    "component", "${var.component}",
+    "env", "${var.env}",
+    "is-production", "${var.is_production ? "true" : "false"}",
+  ), var.tags)}"
+}
