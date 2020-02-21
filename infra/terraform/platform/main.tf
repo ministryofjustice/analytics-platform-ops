11c3cdcda1e29fa7b5ee67d2a5dafa79f6833c0c
Aldo "xoen" Giambelluca <aldo.giambelluca@digital.justice.gov.uk>
2020-02-21 17:06:38 +0000

    buckets archiver IAM role
    Also renamed module as it now contains more than just the bucket but
    resources related to the buckets archiver service.

--- infra/terraform/platform/main.tf
+++ infra/terraform/platform/main.tf
@@ -190,12 +190,14 @@ module "cluster_autoscaler" {
   ]
 }
 
-module "archived_buckets_bucket" {
-  source = "../modules/archived_buckets_bucket"
+module "buckets_archiver" {
+  source = "../modules/buckets_archiver"
 
+  env                 = "${terraform.workspace}"
   name                = "${terraform.workspace}-archived-buckets-data"
   logging_bucket_name = "${data.terraform_remote_state.global.s3_logs_bucket_name}"
-  expiration_days     = 183                                                         # 6 months
+  expiration_days     = 183                                                                                                                                                        # 6 months
+  k8s_worker_role_arn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/nodes.${terraform.workspace}.${data.terraform_remote_state.global.platform_root_domain}"
   region              = "${var.region}"
 
   tags = "${var.tags}"
