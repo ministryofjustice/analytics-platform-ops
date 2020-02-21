74fb169df669023cac96fe5fcc742140e357ae98
Aldo "xoen" Giambelluca <aldo.giambelluca@digital.justice.gov.uk>
2020-02-21 13:27:15 +0000

    S3 bucket where archived buckets data goes
    Creates the `${ENV}-archived-buckets-data` S3 bucket.
    Data from other buckets will be moved here when they're archived.
    
    Key features of this S3 bucket:
    - is private
    - bucket has versioning enabled
    - is encrypted
    - has lifecycle rule which deletes objects after 6 months
      (configurabe)
    - has public access blocks in place
    
    Part of ticket: https://trello.com/c/6VdNPbzz

---
+++ infra/terraform/modules/archived_buckets_bucket/variables.tf
@@ -0,0 +1,24 @@
+variable "name" {
+  type        = "string"
+  description = "name of the 'archived buckets' bucket"
+}
+
+variable "logging_bucket_name" {
+  type        = "string"
+  description = "name of the bucket where logging for the 'archived buckets' bucket will go"
+}
+
+variable "tags" {
+  type        = "map"
+  description = "Tags to attach to the bucket"
+}
+
+variable "region" {
+  type        = "string"
+  description = "Region where the S3 bucket will be created"
+}
+
+variable "expiration_days" {
+  description = "number of days after which the objects (and the older versions) will be deleted"
+  default     = 183
+}
