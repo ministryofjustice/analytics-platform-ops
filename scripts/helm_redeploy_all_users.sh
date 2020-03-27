e286da6e097bbbc87e21e68913453f02e6e4c341
Dhiraj Narwani <dhiraj.narwani@digital.justice.gov.uk>
2020-03-27 16:05:29 +0000

    add delete of kubernetes infra before helm update

--- scripts/helm_redeploy_all_users.sh
+++ scripts/helm_redeploy_all_users.sh
@@ -2,6 +2,7 @@
 
 ENVIRONMENT=""
 HELM_CHART="config-user"
+HELM_CHART_VERSION="0.2.10"
 
 while true; do 
 	read -p "Enter the environment, i.e. dev or alpha, and press [Enter]: " environment
@@ -15,7 +16,8 @@ done
 CHART="../../analytics-platform-helm-charts/charts/${HELM_CHART}"
 RSTUDIO_CHART="../../analytics-platform-helm-charts/charts/rstudio"
 JUPYTER_CHART="../../analytics-platform-helm-charts/charts/jupyter-lab"
-# CONFIG_FILE="analytics-platform-config/chart-env-config/${ENVIRONMENT}/${HELM_CHART}.yml"
+RSTUDIO_CONFIG_FILE="../../analytics-platform-config/chart-env-config/${ENVIRONMENT}/rstudio.yml"
+JUPYTER_CONFIG_FILE="../../analytics-platform-config/chart-env-config/${ENVIRONMENT}/jupyter.yml"
 # USERNAME=$(helm list --max 10000 | grep "${HELM_CHART}" | awk '{print $1}' | sed "s/${HELM_CHART}-//g")
 USERNAME="dhirajnarwanimoj"
 USERNAME_FILE=$PWD/usernames.txt
@@ -39,34 +41,50 @@ redeploy() {
 	set -x
 
 	while IFS= read -r u; do
+		
+		kubectl delete all -l app=jupyter-lab -n user-$u
 
-		RELEASE_NAME=${HELM_CHART}-$u
+		kubectl delete all -l app=rstudio -n user-$u
+
+		kubectl delete all -l chart=$HELM_CHART-$HELM_CHART_VERSION -n user-$u
+
+		if [[ $? -ne 0 ]]; then
+
+			kubectl delete all -l app=jupyter-lab -n user-$u
+
+			kubectl delete all -l app=rstudio -n user-$u
+
+			kubectl delete all -l chart=$HELM_CHART-$HELM_CHART_VERSION -n user-$u
+
+		fi
+
+		RELEASE_NAME=$HELM_CHART-$u
 
 		JUPYTER_RELEASE_NAME=jupyter-lab-$u
 
 		RSTUDIO_RELEASE_NAME=rstudio-$u
 
 		# add -f $CONFIG_FILE if you want to specify a config file
-		helm upgrade --install $JUPYTER_RELEASE_NAME $JUPYTER_CHART --namespace user-$u
+		helm upgrade --install $JUPYTER_RELEASE_NAME $JUPYTER_CHART --set Username=$u --set aws.iamRole=${ENVIRONMENT}_user_$u -f $JUPYTER_CONFIG_FILE --namespace user-$u
 
 		# add -f $CONFIG_FILE if you want to specify a config file
-		helm upgrade --install $RSTUDIO_RELEASE_NAME $RSTUDIO_CHART --namespace user-$u
+		helm upgrade --install $RSTUDIO_RELEASE_NAME $RSTUDIO_CHART --set username=$u --set aws.iamRole=${ENVIRONMENT}_user_$u -f $RSTUDIO_CONFIG_FILE --namespace user-$u
 
 		# add -f $CONFIG_FILE if you want to specify a config file
 		helm upgrade --install $RELEASE_NAME $CHART --set Username=$u --namespace user-$u
-				
-        if [[ $? -ne 0 ]]; then
 
-			 sleep 120
+		if [[ $? -ne 0 ]]; then
 
-			 # add -f $CONFIG_FILE if you want to specify a config file
-			 helm upgrade --install $JUPYTER_RELEASE_NAME $JUPYTER_CHART --namespace user-$u
+			sleep 120
 
-			 # add -f $CONFIG_FILE if you want to specify a config file
-			 helm upgrade --install $RSTUDIO_RELEASE_NAME $RSTUDIO_CHART --namespace user-$u
+			# add -f $CONFIG_FILE if you want to specify a config file
+			helm upgrade --install $JUPYTER_RELEASE_NAME $JUPYTER_CHART --set Username=$u --set aws.iamRole=${ENVIRONMENT}_user_$u -f $JUPYTER_CONFIG_FILE --namespace user-$u
 
-			 # add -f $CONFIG_FILE if you want to specify a config file
-			 helm upgrade --install $RELEASE_NAME $CHART --set Username=$u --namespace user-$u
+			# add -f $CONFIG_FILE if you want to specify a config file
+			helm upgrade --install $RSTUDIO_RELEASE_NAME $RSTUDIO_CHART --set Username=$u --set aws.iamRole=${ENVIRONMENT}_user_$u -f $RSTUDIO_CONFIG_FILE --namespace user-$u
+
+			# add -f $CONFIG_FILE if you want to specify a config file
+			helm upgrade --install $RELEASE_NAME $CHART --set Username=$u --namespace user-$u
 
 		fi
 
