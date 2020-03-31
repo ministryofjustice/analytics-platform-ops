43c2fda6497cfddb5e189d835907e44e7190a463
Dhiraj Narwani <dhiraj.narwani@digital.justice.gov.uk>
2020-03-31 17:42:33 +0100

    remove kubectl delete all and add flag --recreate-pods

--- infra/terraform/modules/aws_vpc/routes.tf
+++ infra/terraform/modules/aws_vpc/routes.tf
@@ -8,9 +8,9 @@ resource "aws_route_table" "dmz" {
 }
 
 resource "aws_route" "dmz" {
-  route_table_id         = "${aws_route_table.dmz.id}"
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = "${aws_internet_gateway.igw.id}"
+  route_table_id         = "${var.route_table_id}"
+  destination_cidr_block = "${var.sfdsf}"
+  gateway_id             = ""
 }
 
 resource "aws_route_table" "private" {
--- scripts/helm_redeploy_all_users.sh
+++ scripts/helm_redeploy_all_users.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 
 ENVIRONMENT=""
+
 HELM_CHART="config-user"
-HELM_CHART_VERSION="0.2.10"
 
 while true; do 
 	read -p "Enter the environment, i.e. dev or alpha, and press [Enter]: " environment
@@ -19,7 +19,7 @@ JUPYTER_CHART="../../analytics-platform-helm-charts/charts/jupyter-lab"
 RSTUDIO_CONFIG_FILE="../../analytics-platform-config/chart-env-config/${ENVIRONMENT}/rstudio.yml"
 JUPYTER_CONFIG_FILE="../../analytics-platform-config/chart-env-config/${ENVIRONMENT}/jupyter.yml"
 # USERNAME=$(helm list --max 10000 | grep "${HELM_CHART}" | awk '{print $1}' | sed "s/${HELM_CHART}-//g")
-USERNAME="dhirajnarwanimoj"
+USERNAME="mamonu"
 USERNAME_FILE=$PWD/usernames.txt
 
 stat $CHART || exit
@@ -27,7 +27,6 @@ stat $CHART || exit
 declare -a USERNAMES=$USERNAME
 
 username_file() {
-
 	for i in $USERNAMES; do 
 		echo ${i[@]} >> $USERNAME_FILE
 		shift
@@ -37,54 +36,24 @@ username_file() {
 username_file
 
 redeploy() {
-
 	set -x
 
 	while IFS= read -r u; do
-		
-		kubectl delete deployment -l app=jupyter-lab -n user-$u
-
-		kubectl delete deployment -l app=rstudio -n user-$u
-
-		if [[ $? -ne 0 ]]; then
-
-			kubectl delete deployment -l app=jupyter-lab -n user-$u
-
-			kubectl delete deployment -l app=rstudio -n user-$u
-		fi
-
 		RELEASE_NAME=$HELM_CHART-$u
-
 		JUPYTER_RELEASE_NAME=jupyter-lab-$u
-
 		RSTUDIO_RELEASE_NAME=rstudio-$u
 
-		# add -f $CONFIG_FILE if you want to specify a config file
-		helm upgrade --install $JUPYTER_RELEASE_NAME $JUPYTER_CHART --set Username=$u --set aws.iamRole=${ENVIRONMENT}_user_$u -f $JUPYTER_CONFIG_FILE --namespace user-$u
+		helm upgrade --install $JUPYTER_RELEASE_NAME $JUPYTER_CHART --recreate-pods --set Username=$u --set aws.iamRole=${ENVIRONMENT}_user_$u -f $JUPYTER_CONFIG_FILE --namespace user-$u
+		helm upgrade --install $RSTUDIO_RELEASE_NAME $RSTUDIO_CHART --force --recreate-pods --set username=$u --set aws.iamRole=${ENVIRONMENT}_user_$u -f $RSTUDIO_CONFIG_FILE --namespace user-$u
+		helm upgrade --install $RELEASE_NAME $CHART --set Username=$u --recreate-pods --namespace user-$u
 
-		# add -f $CONFIG_FILE if you want to specify a config file
-		helm upgrade --install $RSTUDIO_RELEASE_NAME $RSTUDIO_CHART --set username=$u --set aws.iamRole=${ENVIRONMENT}_user_$u -f $RSTUDIO_CONFIG_FILE --namespace user-$u
-
-		# add -f $CONFIG_FILE if you want to specify a config file
-		helm upgrade --install $RELEASE_NAME $CHART --set Username=$u --namespace user-$u
-
+		# If the above helm upgrade fails due to Timeout/TLS handshake issues then retry the upgrade
 		if [[ $? -ne 0 ]]; then
-
-			sleep 120
-
-			# add -f $CONFIG_FILE if you want to specify a config file
-			helm upgrade --install $JUPYTER_RELEASE_NAME $JUPYTER_CHART --set Username=$u --set aws.iamRole=${ENVIRONMENT}_user_$u -f $JUPYTER_CONFIG_FILE --namespace user-$u
-
-			# add -f $CONFIG_FILE if you want to specify a config file
-			helm upgrade --install $RSTUDIO_RELEASE_NAME $RSTUDIO_CHART --set Username=$u --set aws.iamRole=${ENVIRONMENT}_user_$u -f $RSTUDIO_CONFIG_FILE --namespace user-$u
-
-			# add -f $CONFIG_FILE if you want to specify a config file
-			helm upgrade --install $RELEASE_NAME $CHART --set Username=$u --namespace user-$u
-
+			helm upgrade --install $JUPYTER_RELEASE_NAME $JUPYTER_CHART --recreate-pods --set Username=$u --set aws.iamRole=${ENVIRONMENT}_user_$u -f $JUPYTER_CONFIG_FILE --namespace user-$u
+			helm upgrade --install $RSTUDIO_RELEASE_NAME $RSTUDIO_CHART --force --recreate-pods --set username=$u --set aws.iamRole=${ENVIRONMENT}_user_$u -f $RSTUDIO_CONFIG_FILE --namespace user-$u
+			helm upgrade --install $RELEASE_NAME $CHART --recreate-pods --set Username=$u --namespace user-$u
 		fi
-
-	done <$USERNAME_FILE
-    
+	done <$USERNAME_FILE 
 }
 
 redeploy
