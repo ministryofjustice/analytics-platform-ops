6e7822c04519f84dad292e0b3aad1a0f13c0d9a1
Dhiraj Narwani <dhiraj.narwani@digital.justice.gov.uk>
2020-03-31 17:44:22 +0100

    revert code

--- infra/terraform/modules/aws_vpc/routes.tf
+++ infra/terraform/modules/aws_vpc/routes.tf
@@ -10,7 +10,7 @@ resource "aws_route_table" "dmz" {
 resource "aws_route" "dmz" {
   route_table_id         = "${aws_route_table.dmz.id}"
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = ""
+  gateway_id             = "${aws_internet_gateway.igw.id}"
 }
 
 resource "aws_route_table" "private" {
