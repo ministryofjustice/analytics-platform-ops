710ee3e8d7f5d9d24721c0e6771d6f3d1db275d0
andylightfoot <62545579+andylightfoot@users.noreply.github.com>
2020-06-15 12:13:29 +0100

    Add Cloudwatch alerts for Kubernetes master and bastion ELBs (#324)
    * Add Cloudwatch alert for Kubernetes master and bastion ELBs
    * Create SNS topic for AP infra alerts

---
+++ infra/terraform/modules/sns_alerts/variables.tf
@@ -0,0 +1,14 @@
+variable "name" {
+  type        = "string"
+  description = "The common name given to resources"
+}
+
+variable "email" {
+  type        = "string"
+  description = "email address where alerts are sent to"
+}
+
+variable "tags" {
+  type        = "map"
+  description = "Tags to attach to resources"
+}
