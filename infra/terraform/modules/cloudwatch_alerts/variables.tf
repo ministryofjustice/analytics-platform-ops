421f30999a7c88ee5b6d6a7328c1a9a6571e29ae
andylightfoot <62545579+andylightfoot@users.noreply.github.com>
2020-06-16 15:53:37 +0100

    Update Softnas cpu alerts so that only send alerts when we need to investigate (#327)
    * Update Softnas cpu alerts so that only alert when need to investigate
    
    * Add comment to explain logic for softnas monitoring
    
    Co-authored-by: alightfoot <andrewlightfoot@Andrews-MacBook-Pro-2.local>

--- infra/terraform/modules/cloudwatch_alerts/variables.tf
+++ infra/terraform/modules/cloudwatch_alerts/variables.tf
@@ -32,3 +32,18 @@ variable "tags" {
   type        = "map"
   description = "Tags to attach to resources"
 }
+
+variable "period" {
+  description = "The period in seconds over which the specified statistic is applied."
+  default     = 300
+}
+
+variable "evaluation_periods" {
+  description = "The number of periods over which data is compared to the specified threshold."
+  default     = 3
+}
+
+variable "datapoints_to_alarm" {
+  description = "The number of datapoints that must be breaching to trigger the alarm."
+  default     = 3
+}
