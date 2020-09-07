a022644f9e039b603c5d9e3278b0ed9292159959
Dan Webb <dan.webb@damacus.io>
2020-09-07 14:33:58 +0100

    Split out the providers configuration
    This is in preparation for the upcoming Terraform 0.13 block which greatly increases the possible block complexity

--- infra/terraform/platform/main.tf
+++ infra/terraform/platform/main.tf
@@ -1,17 +1,3 @@
-terraform {
-  backend "s3" {
-    bucket               = "terraform.analytics.justice.gov.uk"
-    workspace_key_prefix = "platform:"
-    key                  = "terraform.tfstate"
-    region               = "eu-west-1"
-  }
-}
-
-provider "aws" {
-  region  = "${var.region}"
-  version = "~> 2.45"
-}
-
 module "user_nfs_softnas" {
   source = "../modules/user_nfs_softnas"
 
