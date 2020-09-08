656262fc7013e1ccae1324a2747c7cbd121dbef5
Dan Webb <dan.webb@damacus.io>
2020-09-08 11:54:59 +0100

    Move IAM policies from inline to iam_policy_documents (#356)
    * Change inline IAM policies to policy_documents
    
    This allows us to format the IAM policy documents cleaner using HCL rather than json directly.
    
    * Move EBS snapshotting into it's own file
    
    Move the EBS snapshot modules into it's own file so we can operate on it easily.

--- infra/terraform/global/iam.tf
+++ infra/terraform/global/iam.tf
@@ -3,24 +3,21 @@ resource "aws_iam_user" "auth0_ses" {
 }
 
 resource "aws_iam_user_policy" "auth0_ses" {
-  name = "auth0_ses_user_policy"
-  user = "${aws_iam_user.auth0_ses.name}"
-
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": [
-        "ses:SendRawEmail",
-        "ses:SendEmail"
-      ],
-      "Effect": "Allow",
-      "Resource": "*"
-    }
-  ]
+  name   = "auth0_ses_user_policy"
+  user   = "${aws_iam_user.auth0_ses.name}"
+  policy = "${data.aws_iam_policy_document.auth0_ses.json}"
 }
-EOF
+
+data "aws_iam_policy_document" "auth0_ses" {
+  statement {
+    effect    = "Allow"
+    resources = ["*"]
+
+    actions = [
+      "ses:SendRawEmail",
+      "ses:SendEmail",
+    ]
+  }
 }
 
 resource "aws_iam_access_key" "auth0_ses" {
@@ -28,77 +25,66 @@ resource "aws_iam_access_key" "auth0_ses" {
 }
 
 resource "aws_iam_role" "softnas" {
-  name = "SoftNAS_HA_IAM"
-
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "ec2.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
+  name               = "SoftNAS_HA_IAM"
+  assume_role_policy = "${data.aws_iam_policy_document.softnas_role.json}"
 }
-EOF
+
+data "aws_iam_policy_document" "softnas_role" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
 }
 
 resource "aws_iam_role_policy" "softnas" {
-  name = "softnas_ha_policy"
-  role = "${aws_iam_role.softnas.id}"
-
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Sid": "Stmt1444200186000",
-      "Effect": "Allow",
-      "Action": [
-        "ec2:ModifyInstanceAttribute",
-        "ec2:DescribeInstances",
-        "ec2:CreateVolume",
-        "ec2:DeleteVolume",
-        "ec2:CreateSnapshot",
-        "ec2:DeleteSnapshot",
-        "ec2:CreateTags",
-        "ec2:DeleteTags",
-        "ec2:AttachVolume",
-        "ec2:DetachVolume",
-        "ec2:DescribeInstances",
-        "ec2:DescribeVolumes",
-        "ec2:DescribeSnapshots",
-
-        "aws-marketplace:MeterUsage",
-
-        "ec2:DescribeRouteTables",
-        "ec2:DescribeAddresses",
-        "ec2:DescribeTags",
-        "ec2:DescribeInstances",
-        "ec2:ModifyNetworkInterfaceAttribute",
-        "ec2:ReplaceRoute",
-        "ec2:CreateRoute",
-        "ec2:DeleteRoute",
-        "ec2:AssociateAddress",
-        "ec2:DisassociateAddress",
-
-        "s3:CreateBucket",
-        "s3:Delete*",
-        "s3:Get*",
-        "s3:List*",
-        "s3:Put*"
-      ],
-      "Resource": [
-        "*"
-      ]
-    }
-  ]
+  name   = "softnas_ha_policy"
+  role   = "${aws_iam_role.softnas.id}"
+  policy = "${data.aws_iam_policy_document.softnas.json}"
 }
-EOF
+
+data "aws_iam_policy_document" "softnas" {
+  statement {
+    effect    = "Allow"
+    sid       = "Stmt1444200186000"
+    resources = ["*"]
+
+    actions = [
+      "ec2:ModifyInstanceAttribute",
+      "ec2:DescribeInstances",
+      "ec2:CreateVolume",
+      "ec2:DeleteVolume",
+      "ec2:CreateSnapshot",
+      "ec2:DeleteSnapshot",
+      "ec2:CreateTags",
+      "ec2:DeleteTags",
+      "ec2:AttachVolume",
+      "ec2:DetachVolume",
+      "ec2:DescribeInstances",
+      "ec2:DescribeVolumes",
+      "ec2:DescribeSnapshots",
+      "aws-marketplace:MeterUsage",
+      "ec2:DescribeRouteTables",
+      "ec2:DescribeAddresses",
+      "ec2:DescribeTags",
+      "ec2:DescribeInstances",
+      "ec2:ModifyNetworkInterfaceAttribute",
+      "ec2:ReplaceRoute",
+      "ec2:CreateRoute",
+      "ec2:DeleteRoute",
+      "ec2:AssociateAddress",
+      "ec2:DisassociateAddress",
+      "s3:CreateBucket",
+      "s3:Delete*",
+      "s3:Get*",
+      "s3:List*",
+      "s3:Put*",
+    ]
+  }
 }
 
 resource "aws_iam_user" "concourse_update_helm_repo" {
@@ -110,23 +96,20 @@ resource "aws_iam_access_key" "concourse_update_helm_repo_access_key" {
 }
 
 resource "aws_iam_user_policy" "concourse_update_helm_repo_policy" {
-  name = "${aws_iam_user.concourse_update_helm_repo.name}_policy"
-  user = "${aws_iam_user.concourse_update_helm_repo.name}"
-
-  policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [{
-        "Sid": "UpdateHelmRepoS3Bucket",
-        "Effect": "Allow",
-        "Action": [
-            "s3:PutObject",
-            "s3:GetObject"
-        ],
-        "Resource": [
-            "arn:aws:s3:::${var.helm_repo_s3_bucket_name}/*"
-        ]
-    }]
+  name   = "${aws_iam_user.concourse_update_helm_repo.name}_policy"
+  user   = "${aws_iam_user.concourse_update_helm_repo.name}"
+  policy = "${data.aws_iam_policy_document.concourse.json}"
 }
-EOF
+
+data "aws_iam_policy_document" "concourse" {
+  statement {
+    effect    = "Allow"
+    sid       = "UpdateHelmRepoS3Bucket"
+    resources = ["arn:aws:s3:::${var.helm_repo_s3_bucket_name}/*"]
+
+    actions = [
+      "s3:PutObject",
+      "s3:GetObject",
+    ]
+  }
 }
