ede82cd405b7cabe15a995110398713028d97597
Dan Webb <dan.webb@damacus.io>
2020-09-08 13:22:59 +0100

    Remove uploads bucket that is maintained by Data Engineering
    This bucket used to be maintained by the Analytical Platform, but now is
    maintained by the data engineering team. This has led to a situation
    where both Pulumi and Terraform are trying to manage the same resource.
    
    This PR removes it from Terraform. We will also remove it from state as
    part of this PR so Terraform forgets about the resource and Pulumi can
    continue to maintain it.

--- infra/terraform/global/s3.tf
+++ infra/terraform/global/s3.tf
@@ -16,24 +16,6 @@ resource "aws_s3_bucket" "kops_state" {
   }
 }
 
-resource "aws_s3_bucket" "uploads" {
-  bucket = "${var.uploads_bucket_name}"
-  region = "${var.region}"
-  acl    = "private"
-
-  versioning {
-    enabled = true
-  }
-
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
-}
-
 resource "aws_s3_bucket" "lookups" {
   bucket = "${var.lookups_bucket_name}"
   region = "${var.region}"
